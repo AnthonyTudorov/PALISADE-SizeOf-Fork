bc94d5ca93608fe3101f5e3ed7a484883b0fd032
Kurt Rohloff <rohloff@njit.edu>
2015-10-04 04:21:43 -0400

    Adding test source files.

---
+++ test/src/Main_TestAll.cpp
@@ -0,0 +1,39 @@
+//#include "UnitTestBinInt.cpp"
+//#include "UnitTestBinVect.cpp"
+//#include "UnitTestBinMat.cpp"
+#include "gtest/gtest.h"
+
+#include "../../src/math/binint.h"
+#include "../../src/math/binmat.h"
+#include "../../src/math/binvect.h"
+#include "../../src/utils/inttypes.h"
+#include "../../src/math/nbtheory.h"
+#include "../../src/lattice/ideals.h"
+#include "../../src/math/distrgen.h"
+#include "../../src/crypto/lwecrypt.h"
+#include "../../src/crypto/lwepre.h"
+#include "../../src/lattice/il2n.h"
+#include "../../src/utils/utilities.h"
+
+/*
+#include "binint.h"
+#include "binmat.h"
+#include "binvect.h"
+#include "inttypes.h"
+#include "nbtheory.h"
+#include "ideals.h"
+#include "distrgen.h"
+#include "lwecrypt.h"
+#include "lwepre.h"
+#include "il2n.h"
+#include "utilities.h"
+*/
+
+using namespace std;
+using namespace lbcrypto;
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
