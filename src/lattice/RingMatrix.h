85295de95c6647a30a8361345c5147d18bc0226b
Kurt Rohloff <rohloff@njit.edu>
2015-11-17 10:07:01 -0500

    Commiting initial RingMatrix capability

---
+++ src/lattice/RingMatrix.h
@@ -0,0 +1,166 @@
+/**
+* @file
+* @author	TPOC: 
+				Dr. Kurt Rohloff <rohloff@njit.edu>,
+			Programmers: 
+				Dr. Yuriy Polyakov <polyakov@njit.edu>
+				Hadi Sajjadpour <ss2959@njit.edu>
+
+* @version 00_03
+*
+* @section LICENSE
+*
+* Copyright (c) 2015, New Jersey Institute of Technology (NJIT)
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this
+* list of conditions and the following disclaimer in the documentation and/or other
+* materials provided with the distribution.
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* @section DESCRIPTION
+*
+* This file contains the ILVector2n Matrix for Matrices of Ring operations.
+*/
+
+#ifndef LBCRYPTO_CRYPTO_RingMatrix_H
+#define LBCRYPTO_CRYPTO_RingMatrix_H
+
+//Includes Section
+#include "il2n.h"
+#include "ideals.h"
+
+/**
+* @namespace lbcrypto
+* The namespace of lbcrypto
+*/
+namespace lbcrypto {
+
+	/**
+	 * @brief Main Ring Matrix class.
+	 * @tparam Element a ring element.
+	 */
+	template <class Element>
+	class RingMatrix {
+	public:
+
+		/**
+		 * Default constructor
+		 */
+		RingMatrix() : m_cryptoParameters(NULL) {}
+
+		/**
+		 * Default constructor
+		 * @param dimension the dimensionality of the matrix
+		 */
+		explicit RingMatrix(usint dimension);
+
+		/**
+		* Copy constructor
+		*/
+		explicit RingMatrix(const RingMatrix<Element> &RingMatrix);
+
+		/**
+		* Moveable copy constructor
+		*/
+		RingMatrix(RingMatrix<Element> &&RingMatrix); 
+
+		/**
+		 * Destructor
+		 */
+		~RingMatrix(){}
+
+		/**
+		* Assignment Operator.
+		*
+		* @param &rhs the copied vector.
+		* @return the resulting vector.
+		*/
+		RingMatrix<Element>& operator=(const RingMatrix<Element> &rhs);
+
+		/**
+		* Moveable Assignment Operator.
+		*
+		* @param &rhs the copied vector.
+		* @return the resulting vector.
+		*/
+		RingMatrix<Element>& operator=(RingMatrix<Element> &&rhs);
+
+		/**
+		* Get a reference to crypto parameters.
+		* @return the crypto parameters.
+		*/
+		const LPCryptoParameters<Element> &GetCryptoParameters() const { return *m_cryptoParameters; }
+
+		/**
+		* Get the dimension.
+		* @return the dimension
+		*/
+		usint GetDimension() const { return m_dimension; }
+
+		/**
+		* Get current estimate of estimate norm
+		* @return the current estimate of RingMatrix norm.
+		*/
+		const BigBinaryInteger &GetNorm() const { return m_norm; }
+
+		/**
+		* Get the element
+		* @param row the row of the index
+		* @param column the column of the index
+		* @return the ring element.
+		*/
+		const Element &GetElement(usint row, usint column) const { return m_element; }
+
+		/**
+		* Sets a reference to crypto parameters.
+		*
+		* @param &cryptoParameters is crypto params passed by reference.
+		*/
+		void SetCryptoParameters(const LPCryptoParameters<Element> &cryptoParameters) { m_cryptoParameters = &cryptoParameters; }
+
+		/**
+		* Sets the data element.
+		*
+		* @param &element is a polynomial ring element.
+		* @param row the row of the index
+		* @param column the column of the index
+		*/
+		void SetElement(const Element &element, usint row, usint column) { m_element = element; }
+
+		/**
+		* Sets the data element.
+		*
+		* @param row the row of the index
+		* @param column the column of the index
+		* @param &element is a polynomial ring element.
+		*/
+		void GetElement(usint row, usint column, const Element &element) { return m_element; }
+	
+	private:
+
+		//pointer to crypto parameters
+		const LPCryptoParameters<Element> *m_cryptoParameters;
+
+		//data element
+		Element m_element;
+
+		//size of matrix
+		usint m_dimension=1;
+
+	};
+
+} // namespace lbcrypto ends
+#endif
