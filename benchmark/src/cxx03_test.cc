420daf9166f1f71829f6e079ce273a9dc13726fe
David Cousins <dcousins@bbn.com>
2015-12-21 16:14:44 -0500

    verison of cxx03_test that may work with c++11

--- benchmark/src/cxx03_test.cc
+++ benchmark/src/cxx03_test.cc
@@ -4,9 +4,33 @@
 #include "benchmark/benchmark.h"
 
 #if __cplusplus >= 201103L
-#error C++11 or greater detected. Should be C++03.
-#endif
 
+//#warning C++11 or greater detected. 
+void BM_empty(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        volatile std::size_t x = state.iterations();
+        ((void)x);
+    }
+}
+BENCHMARK(BM_empty);
+
+template <class T, class U>
+void BM_template(benchmark::State& state) {
+    BM_empty(state);
+}
+BENCHMARK_TEMPLATE(BM_template, int, long);
+
+template <class T>
+void BM_template(benchmark::State& state) {
+    BM_empty(state);
+}
+BENCHMARK_TEMPLATE(BM_template, long);
+BENCHMARK_TEMPLATE(BM_template, int);
+BENCHMARK_TEMPLATE(BM_template, long, int);
+
+BENCHMARK_MAIN()
+#else
+//#warning C++03 detected
 void BM_empty(benchmark::State& state) {
     while (state.KeepRunning()) {
         volatile std::size_t x = state.iterations();
@@ -29,3 +53,4 @@ BENCHMARK_TEMPLATE(BM_template1, long);
 BENCHMARK_TEMPLATE1(BM_template1, int);
 
 BENCHMARK_MAIN()
+#endif
