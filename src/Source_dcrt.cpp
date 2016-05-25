f1355af2d70c3d992c6121c78a88558876899a59
Nishanth <np386@njit.edu>
2016-05-25 12:58:21 -0400

    Issue #227. Moving SHE tests to the Unit test file from Source_dcrt.

--- src/Source_dcrt.cpp
+++ src/Source_dcrt.cpp
@@ -87,7 +87,6 @@ void RingReduceDoubleCRTTest();
 void RingReduceSingleCRTTest();
 void ModReduceNew();
 void ModReduceGyana();
-void KeySwitchTestNewAPI();
 void RingReduceDCRTTest();
 void RingReduceSingleCRTTest();
 /**
@@ -129,8 +128,6 @@ int main() {
 //	ModReduceNew();
 
 //	ModReduceGyana();
-	
-	//KeySwitchTestNewAPI(); 
 
 //	RingReduceDCRTTest();
 //	RingReduceSingleCRTTest();
@@ -1136,101 +1133,6 @@ void ModReduceNew() {
 	algorithm2.m_algorithmLeveledSHE->ModReduce(&cipherText2, &sk2);
 	
 	algorithm2.Decrypt(sk2, cipherText2, &ctxtd);
-
-	cout << "Decrypted after MOD Reduce ILVectorArray2n: \n" << endl;
-	
-	cout << ctxtd<< "\n" << endl;
-	
-}
-
-
-
-void KeySwitchTestNewAPI() {
-
-	usint m = 16;
-
-	const ByteArray plaintext = "M";
-	ByteArrayPlaintextEncoding ptxt(plaintext);
-	ptxt.Pad<ZeroPad>(m/16);
-//	ptxt.Pad<ZeroPad>(m/8);
-
-	float stdDev = 4;
-
-	usint size = 3;
-
-	std::cout << "tower size: " << size << std::endl;
-
-	ByteArrayPlaintextEncoding ctxtd;
-
-	vector<BigBinaryInteger> moduli(size);
-
-	vector<BigBinaryInteger> rootsOfUnity(size);
-
-	BigBinaryInteger q("1");
-	BigBinaryInteger temp;
-	BigBinaryInteger modulus("1");
-
-	for(int i=0; i < size;i++){
-        lbcrypto::NextQ(q, BigBinaryInteger::TWO,m,BigBinaryInteger("4"), BigBinaryInteger("4"));
-		moduli[i] = q;
-		cout << moduli[i] << endl;
-		rootsOfUnity[i] = RootOfUnity(m,moduli[i]);
-		cout << rootsOfUnity[i] << endl;
-		modulus = modulus* moduli[i];
-	
-	}
-
-	/*rootsOfUnity[0] = BigBinaryInteger("10878");
-	rootsOfUnity[1] = BigBinaryInteger("12967");*/
-	
-	cout << "big modulus: " << modulus << endl;
-	DiscreteGaussianGenerator dgg(modulus,stdDev);
-
-	ILDCRTParams params(rootsOfUnity, m, moduli);
-
-//	ILDCRTParams params(rootsOfUnity, m, moduli,modulus1*modulus2);
-
-	LPCryptoParametersLTV<ILVectorArray2n> cryptoParams2;
-//	BigBinaryInteger plaintextm("8");
-	cryptoParams2.SetPlaintextModulus(BigBinaryInteger::TWO);
-//	cryptoParams2.SetPlaintextModulus(plaintextm);
-	cryptoParams2.SetDistributionParameter(stdDev);
-	cryptoParams2.SetRelinWindow(1);
-	cryptoParams2.SetElementParams(params);
-	cryptoParams2.SetDiscreteGaussianGenerator(dgg);
-
-	Ciphertext<ILVectorArray2n> cipherText2;
-	cipherText2.SetCryptoParameters(cryptoParams2);
-
-
-	LPPublicKeyLTV<ILVectorArray2n> pk2(cryptoParams2);
-	LPPrivateKeyLTV<ILVectorArray2n> sk2(cryptoParams2);
-
-	LPPublicKeyLTV<ILVectorArray2n> pk3(cryptoParams2);
-	LPPrivateKeyLTV<ILVectorArray2n> sk3(cryptoParams2);
-
-	std::bitset<FEATURESETSIZE> mask (std::string("1000011"));
-	LPPublicKeyEncryptionSchemeLTV<ILVectorArray2n> algorithm2(mask);
-
-	//LPAlgorithmLTV<ILVectorArray2n> algorithm2;
-
-	algorithm2.KeyGen(&pk2, &sk2);
-	algorithm2.KeyGen(&pk3, &sk3);
-
-	algorithm2.Encrypt(pk2, ptxt, &cipherText2);
-
-	algorithm2.Decrypt(sk2, cipherText2, &ctxtd);
-
-	
-	cout << "Decrypted value ILVectorArray2n: \n" << endl;
-	cout << ctxtd<< "\n" << endl;
-
-	LPKeySwitchHintLTV<ILVectorArray2n> keySwitchHint;
-
-	algorithm2.m_algorithmLeveledSHE->KeySwitchHintGen(sk2,sk3, &keySwitchHint);
-	Ciphertext<ILVectorArray2n> cipherText3(algorithm2.m_algorithmLeveledSHE->KeySwitch(keySwitchHint, cipherText2));
-
-	algorithm2.Decrypt(sk3, cipherText3, &ctxtd);
 
 	cout << "Decrypted after MOD Reduce ILVectorArray2n: \n" << endl;
 	
--- test/src/UnitTestSHE.cpp
+++ test/src/UnitTestSHE.cpp
@@ -41,49 +41,48 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 #include "../../src/lattice/ilvectorarray2n.h"
 #include "../../src/utils/utilities.h"
 
+#include "../../src/crypto/lwecrypt.cpp"
+#include "../../src/crypto/lwefhe.cpp"
+#include "../../src/crypto/lweshe.cpp"
+#include "../../src/crypto/lweautomorph.cpp"
+#include "../../src/crypto/lweahe.cpp"
+#include "../../src/crypto/lwepre.cpp"
+#include "../../src/crypto/ciphertext.cpp"
+
 using namespace std;
 using namespace lbcrypto;
 
-//https://github.com/google/googletest/blob/master/googletest/samples/sample6_unittest.cc
-
 template <class T>
 class UnitTestSHE : public ::testing::Test {
- protected:
-  virtual void SetUp() {
-  }
+  protected:
+    virtual void SetUp() {
+    }
 
-  virtual void TearDown() {
-    // Code here will be called immediately after each test
-    // (right before the destructor).
-  }
+    virtual void TearDown() {
+      // Code here will be called immediately after each test
+      // (right before the destructor).
+    }
+
+    // LPCryptoParametersLTV<ILVectorArray2n>* const cryptoParams;
 };
 
-/*--------------------------------------- TESTING METHODS OF TRANSFORM    --------------------------------------------*/
+typedef ::testing::Types<ILVectorArray2n> Implementations;
 
-// TEST CASE TO TEST POLYNOMIAL MULTIPLICATION USING CHINESE REMAINDER THEOREM
+TYPED_TEST_CASE(UnitTestSHE, Implementations);
 
-TEST(SHEOperations_test, keyswitch_test_single_crt){
-
-}
-
-TEST(SHEOperations_test, keyswitch_test_double_crt){
-
+// Then use TYPED_TEST(TestCaseName, TestName) to define a typed test,
+// similar to TEST_F.
+TYPED_TEST(UnitTestSHE, keyswitch_test_double_crt) {
   usint m = 16;
   const ByteArray plaintext = "M";
   ByteArrayPlaintextEncoding ptxt(plaintext);
   ptxt.Pad<ZeroPad>(m/16);
-//  ptxt.Pad<ZeroPad>(m/8);
 
   float stdDev = 4;
-
   usint size = 3;
-
-  std::cout << "tower size: " << size << std::endl;
-
   ByteArrayPlaintextEncoding ctxtd;
 
   vector<BigBinaryInteger> moduli(size);
-
   vector<BigBinaryInteger> rootsOfUnity(size);
 
   BigBinaryInteger q("1");
@@ -91,65 +90,50 @@ TEST(SHEOperations_test, keyswitch_test_double_crt){
   BigBinaryInteger modulus("1");
 
   for(int i=0; i < size;i++){
-        lbcrypto::NextQ(q, BigBinaryInteger::TWO,m,BigBinaryInteger("4"), BigBinaryInteger("4"));
+    lbcrypto::NextQ(q, BigBinaryInteger::TWO,m,BigBinaryInteger("4"), BigBinaryInteger("4"));
     moduli[i] = q;
-    cout << moduli[i] << endl;
     rootsOfUnity[i] = RootOfUnity(m,moduli[i]);
-    cout << rootsOfUnity[i] << endl;
     modulus = modulus* moduli[i];
-  
   }
 
-  cout << "big modulus: " << modulus << endl;
   DiscreteGaussianGenerator dgg(modulus,stdDev);
-
   ILDCRTParams params(rootsOfUnity, m, moduli);
 
-  // LPCryptoParametersLTV<ILVectorArray2n> cryptoParams2;
-// //  BigBinaryInteger plaintextm("8");
-//   cryptoParams2.SetPlaintextModulus(BigBinaryInteger::TWO);
-// //  cryptoParams2.SetPlaintextModulus(plaintextm);
-//   cryptoParams2.SetDistributionParameter(stdDev);
-//   cryptoParams2.SetRelinWindow(1);
-//   cryptoParams2.SetElementParams(params);
-//   cryptoParams2.SetDiscreteGaussianGenerator(dgg);
+  LPCryptoParametersLTV<ILVectorArray2n> cryptoParams;
+  cryptoParams.SetPlaintextModulus(BigBinaryInteger::TWO);
+  cryptoParams.SetDistributionParameter(stdDev);
+  cryptoParams.SetRelinWindow(1);
+  cryptoParams.SetElementParams(params);
+  cryptoParams.SetDiscreteGaussianGenerator(dgg);
 
-//   Ciphertext<ILVectorArray2n> cipherText2;
-//   cipherText2.SetCryptoParameters(cryptoParams2);
+  Ciphertext<ILVectorArray2n> cipherText;
+  cipherText.SetCryptoParameters(cryptoParams);
 
+  LPPublicKeyLTV<ILVectorArray2n> pk(cryptoParams);
+  LPPrivateKeyLTV<ILVectorArray2n> sk(cryptoParams);
 
-//   LPPublicKeyLTV<ILVectorArray2n> pk2(cryptoParams2);
-//   LPPrivateKeyLTV<ILVectorArray2n> sk2(cryptoParams2);
+  std::bitset<FEATURESETSIZE> mask (std::string("1000011"));
+  LPPublicKeyEncryptionSchemeLTV<ILVectorArray2n> algorithm(mask);
 
-//   LPPublicKeyLTV<ILVectorArray2n> pk3(cryptoParams2);
-//   LPPrivateKeyLTV<ILVectorArray2n> sk3(cryptoParams2);
-
-//   std::bitset<FEATURESETSIZE> mask (std::string("1000011"));
-//   LPPublicKeyEncryptionSchemeLTV<ILVectorArray2n> algorithm2(mask);
-
-//   //LPAlgorithmLTV<ILVectorArray2n> algorithm2;
-
-//   algorithm2.KeyGen(&pk2, &sk2);
-//   algorithm2.KeyGen(&pk3, &sk3);
-
-//   algorithm2.Encrypt(pk2, ptxt, &cipherText2);
-
-//   algorithm2.Decrypt(sk2, cipherText2, &ctxtd);
-
+  algorithm.KeyGen(&pk, &sk);
+  algorithm.Encrypt(pk, ptxt, &cipherText);
+  ctxtd.Unpad<ZeroPad>();
+  algorithm.m_algorithmLeveledSHE->ModReduce(&cipherText, &sk);
+  algorithm.Decrypt(sk, cipherText, &ctxtd);
   
-//   cout << "Decrypted value ILVectorArray2n: \n" << endl;
-//   cout << ctxtd<< "\n" << endl;
+  EXPECT_EQ(plaintext, ctxtd.GetData());
+}
 
-//   LPKeySwitchHintLTV<ILVectorArray2n> keySwitchHint;
+/*--------------------------------------- TESTING METHODS OF TRANSFORM    --------------------------------------------*/
 
-//   algorithm2.m_algorithmLeveledSHE->KeySwitchHintGen(sk2,sk3, &keySwitchHint);
-//   Ciphertext<ILVectorArray2n> cipherText3(algorithm2.m_algorithmLeveledSHE->KeySwitch(keySwitchHint, cipherText2));
+// TEST CASE TO TEST POLYNOMIAL MULTIPLICATION USING CHINESE REMAINDER THEOREM
 
-//   algorithm2.Decrypt(sk3, cipherText3, &ctxtd);
-
-//   cout << "Decrypted after MOD Reduce ILVectorArray2n: \n" << endl;
+/*TEST(SHEOperations_test, keyswitch_test_single_crt){
   
-//   cout << ctxtd<< "\n" << endl;
+}
+
+TEST(SHEOperations_test, keyswitch_test_double_crt){
+  
 }
 
 TEST(SHEOperations_test, mod_reduce_test_single_crt){
@@ -174,5 +158,5 @@ TEST(SHEOperations_test, decompose_test_single_crt){
 
 TEST(SHEOperations_test, decompose_test_double_crt){
 
-}
+}*/
 
