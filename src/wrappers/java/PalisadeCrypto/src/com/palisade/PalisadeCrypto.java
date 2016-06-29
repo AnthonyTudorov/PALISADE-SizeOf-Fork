7fd45e116b26df3ea50fb0aa009a02c916bc8ca0
Jerry Ryan <gwryan@njit.edu>
2016-06-29 09:02:24 -0400

    improve the comments in the Java wrapper classes

--- src/wrappers/java/PalisadeCrypto/src/com/palisade/PalisadeCrypto.java
+++ src/wrappers/java/PalisadeCrypto/src/com/palisade/PalisadeCrypto.java
@@ -13,10 +13,19 @@ public class PalisadeCrypto {
 	
 	private long	object;
 	
+	/**
+	 * Constructs a PalisadeCrypto with default parameters
+	 * @throws InstantiationException
+	 */
 	public PalisadeCrypto() throws InstantiationException {
 		this(DEFAULTPARMS);
 	}
 
+	/**
+	 * 
+	 * @param parmset - JSON string of parameters for creating a PalisadeCrypto
+	 * @throws InstantiationException
+	 */
 	public PalisadeCrypto(String parmset) throws InstantiationException {
 		object = openPalisadeCrypto(parmset.getBytes());
 		if( object == 0 ) {
@@ -28,17 +37,70 @@ public class PalisadeCrypto {
 		return object;
 	}
 	
+	/**
+	 * 
+	 * @param id - string to identify the keypair
+	 * @return a PalisadeKeypair with a public and private key JSON array
+	 */
 	public native PalisadeKeypair generatePalisadeKeyPair(String id);
+	
+	/**
+	 * 
+	 * @param id - string to identify the key
+	 * @param publicKey - subscriber's public key
+	 * @param privateKey - publisher's private key
+	 * @return - serialized JSON array containing the evaluation key
+	 */
 	public native byte[] generatePalisadeEvalKey(String id, byte[] publicKey, byte[] privateKey);
 
+	/**
+	 * Sets the key to be used for Encryption by deserializing the given key bytes
+	 * @param key - serialized public key
+	 * @return true on success
+	 */
 	public native boolean setPublicKey(byte[] key);
+	
+	/**
+	 * Sets the key to be used for Decryption by deserializing the given key bytes
+	 * @param key - serialized private key
+	 * @return
+	 */
 	public native boolean setPrivateKey(byte[] key);
+	
+	/**
+	 * Sets the key to be used for Re-encryption by deserializing the given key bytes
+	 * @param key
+	 * @return
+	 */
 	public native boolean setEvalKey(byte[] key);
 	
+	/**
+	 * Encrypt using the key set in #setPublicKey
+	 * @param id - identifies the encryption
+	 * @param cleartext - the text to be encrypted
+	 * @return - JSON serialization of the ciphertext
+	 */
 	public native byte[] encrypt(String id, byte[] cleartext);
+	
+	/**
+	 * Re-Encrypt using the key set in #setEvalKey
+	 * @param id - identifies the encryption
+	 * @param ciphertext - the JSON serialization of the payload to be re-encrypted
+	 * @return - JSON serialization of the ciphertext
+	 */
 	public native byte[] reEncrypt(String id, byte[] ciphertext);
+	
+	/**
+	 * Decrypt using the key set in #setPrivateKey
+	 * @param id - identifies the encryption
+	 * @param ciphertext - the JSON serialization of the payload to be decrypted
+	 * @return - cleartext byte array
+	 */
 	public native byte[] decrypt(String id, byte[] ciphertext);
 	
+	/**
+	 * Finish with this PalisadeCrypto instance
+	 */
 	public void close() {
 		closePalisadeCrypto();
 		object = 0;
@@ -53,7 +115,16 @@ public class PalisadeCrypto {
 		super.finalize();
 	}
 
+	/**
+	 * Creates a new object for this PalisadeCrypto in the JNI layer
+	 * @param parmset - JSON representation of parameters
+	 * @return - reference to the JNI object
+	 */
 	private native long openPalisadeCrypto(byte[] parmset);
+	
+	/**
+	 * Close the JNI layer for this object
+	 */
 	private native void closePalisadeCrypto();
 	
 	public native byte[] getPalisadeErrorDescription();
