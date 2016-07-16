59534dbd79a06cfc701bb944f795e50eaad94434
Yuriy Polyakov <polyakov@njit.edu>
2016-07-15 22:52:12 -0400

    another change related thread_local

--- src/lib/math/distributiongenerator.cpp
+++ src/lib/math/distributiongenerator.cpp
@@ -5,7 +5,7 @@
 namespace lbcrypto {
 
 std::once_flag DistributionGenerator::m_flag;
-std::shared_ptr<std::mt19937> DistributionGenerator::m_prng = nullptr;
+thread_local std::shared_ptr<std::mt19937> DistributionGenerator::m_prng = nullptr;
 
 DistributionGenerator::DistributionGenerator () {
 	// Currently does nothing, but here for forward compatibility.
