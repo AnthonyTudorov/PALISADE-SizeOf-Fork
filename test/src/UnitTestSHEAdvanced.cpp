a677ac8dc82fd50785ee5fc582f18da491ffbe44
Hadi Sajjadpour <ss2959@njit.edu>
2016-08-26 17:22:30 -0400

    removed commented code

--- test/src/UnitTestSHEAdvanced.cpp
+++ test/src/UnitTestSHEAdvanced.cpp
@@ -306,9 +306,6 @@ TEST_F(UnitTestSHEAdvanced, test_eval_add) {
 	
 }
 
-
-
-
 /*Testing Parameter selection. The test will check if generated parameters are greater than the following thresholds:
 * The first modulus generated needs to be greater than q1 > 4pr sqrt(n) w. Where
 * p is the plaintext modulus
@@ -512,9 +509,7 @@ TEST_F(UnitTestSHEAdvanced, test_composed_eval_mult_two_towers) {
 
 }
 
-
 TEST_F(UnitTestSHEAdvanced, test_eval_mult_single_crt) {
-	ILVector2n::DestroyPreComputedSamples();
 
 	usint m = 16;
 
@@ -715,4 +710,81 @@ TEST_F(UnitTestSHEAdvanced, test_eval_mult_double_crt) {
 	EXPECT_EQ(results.at(2), 3);
 
 
+}
+
+TEST_F(UnitTestSHEAdvanced, test_eval_add_single_crt) {
+	usint m = 16;
+
+	float stdDev = 4;
+
+	BigBinaryInteger q("1");
+	BigBinaryInteger temp;
+
+	lbcrypto::NextQ(q, BigBinaryInteger::FIVE, m, BigBinaryInteger("4000"), BigBinaryInteger("400"));
+	DiscreteGaussianGenerator dgg(stdDev);
+	BigBinaryInteger rootOfUnity(RootOfUnity(m, q));
+	ILParams params(m, q, RootOfUnity(m, q));
+
+	//Precomputations for DGG
+	ILVector2n::PreComputeDggSamples(dgg, params);
+
+	LPCryptoParametersLTV<ILVector2n> cryptoParams;
+	cryptoParams.SetPlaintextModulus(BigBinaryInteger::FIVE + BigBinaryInteger::THREE); // Set plaintext modulus.
+	cryptoParams.SetDistributionParameter(stdDev);          // Set the noise parameters.
+	cryptoParams.SetRelinWindow(1);						   // Set the relinearization window
+	cryptoParams.SetElementParams(params);                // Set the initialization parameters.
+	cryptoParams.SetDiscreteGaussianGenerator(dgg);         // Create the noise generator
+
+	Ciphertext<ILVector2n> cipherText;
+	cipherText.SetCryptoParameters(&cryptoParams);
+
+	//Initialize the public key containers.
+	LPPublicKeyLTV<ILVector2n> pk(cryptoParams);
+	LPPrivateKeyLTV<ILVector2n> sk(cryptoParams);
+
+	std::vector<usint> vectorOfInts1(8);
+	vectorOfInts1.at(0) = 2;
+	vectorOfInts1.at(1) = 3;
+	vectorOfInts1.at(2) = 1;
+	vectorOfInts1.at(3) = 4;
+	std::fill(vectorOfInts1.begin() + 4, vectorOfInts1.end(), 0);
+	IntPlaintextEncoding intArray1(vectorOfInts1);
+
+	std::vector<usint> vectorOfInts2(8);
+	vectorOfInts2.at(0) = 3;
+	vectorOfInts2.at(1) = 6;
+	vectorOfInts2.at(2) = 3;
+	vectorOfInts2.at(3) = 1;
+	IntPlaintextEncoding intArray2(vectorOfInts2);
+	std::fill(vectorOfInts2.begin() + 4, vectorOfInts2.end(), 0);
+
+	LPPublicKeyEncryptionSchemeLTV<ILVector2n> algorithm;
+	algorithm.Enable(ENCRYPTION);
+	algorithm.Enable(SHE);
+	algorithm.Enable(LEVELEDSHE);
+
+	algorithm.KeyGen(&pk, &sk);
+
+	vector<Ciphertext<ILVector2n>> ciphertext1;
+	vector<Ciphertext<ILVector2n>> ciphertext2;
+
+	CryptoUtility<ILVector2n>::Encrypt(algorithm, pk, intArray1, &ciphertext1, false);
+	CryptoUtility<ILVector2n>::Encrypt(algorithm, pk, intArray2, &ciphertext2, false);
+
+	Ciphertext<ILVector2n> cResult(ciphertext1.at(0));
+
+	algorithm.EvalAdd(ciphertext1.at(0), ciphertext2.at(0), &cResult);
+
+	vector<Ciphertext<ILVector2n>> ciphertextResults(1);
+	ciphertextResults.at(0) = cResult;
+	IntPlaintextEncoding results;
+
+	CryptoUtility<ILVector2n>::Decrypt(algorithm, sk, ciphertextResults, &results, false);
+
+	EXPECT_EQ(results.at(0), 5);
+	EXPECT_EQ(results.at(1), 0);
+	EXPECT_EQ(results.at(2), 4);
+	EXPECT_EQ(results.at(3), 5);
+
+	ILVector2n::DestroyPreComputedSamples();
 }
