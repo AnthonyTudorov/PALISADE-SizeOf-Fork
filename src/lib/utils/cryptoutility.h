7dbe22cfe8408bab95245e8bd2ab6ad34be3798c
Jerry Ryan <gwryan@gmail.com>
2016-09-25 22:35:08 -0400

    leaktester and source_json works. no ser/deser yet

--- src/lib/utils/cryptoutility.h
+++ src/lib/utils/cryptoutility.h
@@ -287,7 +287,7 @@ public:
 		vector<shared_ptr<Ciphertext<Element>>> *newCiphertext)
 	{
 		for (int i = 0; i < ciphertext.size(); i++) {
-			newCiphertext->push_back( scheme.KeySwitch(keySwitchHint, ciphertext.at(i)) );
+			newCiphertext->push_back( scheme.KeySwitch(keySwitchHint, *ciphertext.at(i)) );
 		}
 	}
 
@@ -301,7 +301,7 @@ public:
 		vector<shared_ptr<Ciphertext<Element>>> *ciphertext)
 	{
 		for (int i = 0; i < ciphertext->size(); i++) {
-			scheme.ModReduce(ciphertext->at(i));
+			scheme.ModReduce(&ciphertext->at(i));
 		}
 	}
 
@@ -319,7 +319,7 @@ public:
 
 	{
 		for (int i = 0; i < ciphertext->size(); i++) {
-			scheme.RingReduce(&ciphertext->at(i), keySwitchHint);
+			scheme.RingReduce(&(*ciphertext->at(i)), keySwitchHint);
 		}
 	}
 
