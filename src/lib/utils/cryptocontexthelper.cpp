d45f2747b951cdb8ae969e620c13f88f61090851
Jerry Ryan <gwryan@njit.edu>
2016-10-07 18:06:33 -0400

    consolidating the demo apps into a smaller list

--- src/lib/utils/cryptocontexthelper.cpp
+++ src/lib/utils/cryptocontexthelper.cpp
@@ -224,8 +224,6 @@ CryptoContextHelper<Element>::getNewContextFromSerialization(const Serialized& s
 	return emptyCtx;
 }
 
-
-
 template <class Element>
 CryptoContext<Element>
 CryptoContextHelper<Element>::getNewContext(const string parmset)
