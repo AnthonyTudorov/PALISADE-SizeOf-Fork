33ff46d9b29d1cef0e86b03e5483b847aba9ebed
GYANA <grs22@njit.edu>
2016-10-21 03:11:17 -0400

    packed ciphertext encoding added

---
+++ src/lib/encoding/packedintplaintextencoding.cpp
@@ -0,0 +1,177 @@
+/**
+ * @file
+ * @author  TPOC: Dr. Kurt Rohloff <rohloff@njit.edu>,
+ *	Programmers: Jerry Ryan <gwryan@njit.edu>
+ * @version 00_03
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2015, New Jersey Institute of Technology (NJIT)
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @section DESCRIPTION
+ *
+ * This code provides a int array abstraction.
+ *
+ */
+
+#include "packedintplaintextencoding.h"
+
+namespace lbcrypto {
+
+void PackedIntPlaintextEncoding::Encode(const BigBinaryInteger& modulus, ILVectorArray2n *element, size_t startFrom, size_t length) const{
+	//TODO - OPTIMIZE CODE. Please take a look at line 114 temp.SetModulus
+	ILVector2n encodedSingleCrt = element->GetElementAtIndex(0);
+
+	Encode(modulus, &encodedSingleCrt, startFrom, length);
+	BigBinaryVector tempBBV(encodedSingleCrt.GetValues());
+
+	std::vector<ILVector2n> encodeValues;
+	encodeValues.reserve(element->GetNumOfElements());
+
+	for (usint i = 0; i<element->GetNumOfElements(); i++) {
+		ILParams ilparams(element->GetElementAtIndex(i).GetCyclotomicOrder(), element->GetElementAtIndex(i).GetModulus(), element->GetElementAtIndex(i).GetRootOfUnity());
+		ILVector2n temp(ilparams);
+		tempBBV = encodedSingleCrt.GetValues();
+		tempBBV.SetModulus(ilparams.GetModulus());
+		temp.SetValues(tempBBV, encodedSingleCrt.GetFormat());
+		temp.SignedMod(ilparams.GetModulus());
+		encodeValues.push_back(temp);
+	}
+
+	ILVectorArray2n elementNew(encodeValues);
+	*element = elementNew;
+
+}
+
+
+void PackedIntPlaintextEncoding::Decode(const BigBinaryInteger& modulus, ILVectorArray2n *ilVectorArray2n){
+	bool dbg_flag = false;
+	DEBUG("in Int DECODE IlVectorArray2n");
+
+	ILVector2n interpolatedDecodedValue = ilVectorArray2n->InterpolateIlArrayVector2n();
+	DEBUG("A");
+	Decode(modulus, &interpolatedDecodedValue);
+	DEBUG("B");
+	BigBinaryVector tempBBV(interpolatedDecodedValue.GetValues());
+	DEBUG("C");
+
+
+	std::vector<ILVector2n> encodeValues;
+	encodeValues.reserve(ilVectorArray2n->GetNumOfElements());
+	DEBUG("D");
+	for (usint i = 0; i<ilVectorArray2n->GetNumOfElements(); i++) {
+	  		    DEBUG("i "<<i);
+		ILParams ilparams(ilVectorArray2n->GetElementAtIndex(i).GetCyclotomicOrder(), ilVectorArray2n->GetElementAtIndex(i).GetModulus(), ilVectorArray2n->GetElementAtIndex(i).GetRootOfUnity());
+		DEBUG("a");
+		ILVector2n temp(ilparams);
+		DEBUG("b");
+		tempBBV = interpolatedDecodedValue.GetValues();
+		DEBUG("c");
+		tempBBV.SetModulus(ilparams.GetModulus());
+		DEBUG("d");
+		temp.SetValues(tempBBV, interpolatedDecodedValue.GetFormat());
+		DEBUG("e");
+		temp.SignedMod(ilparams.GetModulus());
+		DEBUG("f");
+		encodeValues.push_back(temp);
+		DEBUG("g");
+	}
+
+	ILVectorArray2n elementNew(encodeValues);
+	*ilVectorArray2n = elementNew;
+}
+
+
+void PackedIntPlaintextEncoding::Encode(const BigBinaryInteger &modulus, ILVector2n *ilVector, size_t startFrom, size_t length) const
+{
+	int padlen = 0;
+	uint32_t mod = modulus.ConvertToInt();
+	usint m = ilVector->GetCyclotomicOrder();
+	if (mod%m != 1) {
+		throw std::logic_error("Cannot pack ciphertext");
+	}
+
+	BigBinaryInteger rootOfUnity(RootOfUnity(m, modulus));
+	std::vector<BigBinaryInteger> aI;
+	aI.reserve(m / 2);
+	for (usint i = 0; i < m / 2; i++) {
+		aI.push_back(rootOfUnity.ModExp(BigBinaryInteger(2 * i + 1), modulus));
+	}
+
+	if( length == 0 ) length = this->size();
+
+	// length is usually chunk size; if start + length would go past the end of the item, add padding
+	if( (startFrom + length) > this->size() ) {
+		padlen = (startFrom + length) - this->size();
+		length = length - padlen;
+	}
+
+	BigBinaryVector temp(ilVector->GetParams().GetCyclotomicOrder()/2,ilVector->GetModulus());
+
+	Format format = COEFFICIENT;
+
+	for (usint i = 0; i < length; i++) {
+		uint32_t entry = this->at(i + startFrom);
+		if( entry >= mod )
+			throw std::logic_error("Cannot encode integer at position " + std::to_string(i) + " because it is >= plaintext modulus " + std::to_string(mod));
+		BigBinaryInteger Val = BigBinaryInteger( entry );
+		temp.SetValAtIndex(i, Val);
+	}
+
+	BigBinaryInteger Num = modulus - BigBinaryInteger::ONE;
+	for( usint i=0; i<padlen; i++ ) {
+		temp.SetValAtIndex(i+length, Num);
+		if( i == 0 )
+			Num = BigBinaryInteger::ZERO;
+	}
+
+	ilVector->SetValues(temp,format);
+}
+
+void PackedIntPlaintextEncoding::Decode(const BigBinaryInteger &modulus,  ILVector2n *ilVector) {
+	*ilVector = ilVector->SignedMod(modulus);
+	std::vector<uint32_t> intArray(ilVector->GetValues().GetLength());
+	for (usint i = 0; i<ilVector->GetValues().GetLength(); i++) {
+		this->push_back( ilVector->GetValues().GetValAtIndex(i).ConvertToInt() );
+	}
+}
+
+void
+PackedIntPlaintextEncoding::Unpad(const BigBinaryInteger &modulus)
+{
+	uint32_t marker = modulus.ConvertToInt() - 1;
+	usint nPadding = 0;
+	for (sint i = this->size() - 1; i >= 0; --i) {
+		nPadding++;
+		if (this->at(i) == marker) {
+			break;
+		}
+	}
+	this->resize(this->size() - nPadding, 0);
+}
+
+size_t
+PackedIntPlaintextEncoding::GetChunksize(const usint cyc, const BigBinaryInteger&) const
+{
+	return cyc/2;
+}
+
+}
---
+++ src/lib/encoding/packedintplaintextencoding.h
@@ -0,0 +1,103 @@
+/**
+ * @file
+ * @author  TPOC: Dr. Kurt Rohloff <rohloff@njit.edu>,
+ *	Programmers: Dr. Yuriy Polyakov, <polyakov@njit.edu>, Gyana Sahu <grs22@njit.edu>,
+ *	Kevin King <4kevinking@gmail.com>
+ * @version 00_03
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2015, New Jersey Institute of Technology (NJIT)
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @section DESCRIPTION
+ *
+ * This code provides a int array abstraction.
+ *
+ */
+#ifndef LBCRYPTO_UTILS_PACKED_INTPLAINTEXTENCODING_H
+#define LBCRYPTO_UTILS_PACKED_INTPLAINTEXTENCODING_H
+
+#include "inttypes.h"
+#include <vector>
+#include <initializer_list>
+#include "plaintext.h"
+
+namespace lbcrypto {
+
+/**
+ * @brief Type used for representing IntArray types.
+ * Provides conversion functions to vector<uint32_t>
+ */
+class PackedIntPlaintextEncoding : public Plaintext, public std::vector<uint32_t> {
+public:
+	PackedIntPlaintextEncoding(std::vector<uint32_t>::const_iterator sIter, std::vector<uint32_t>::const_iterator eIter)
+    	: std::vector<uint32_t>(std::vector<uint32_t>(sIter, eIter)) {}
+
+	PackedIntPlaintextEncoding(const std::vector<uint32_t> &rhs) : std::vector<uint32_t>(rhs) {}
+
+	PackedIntPlaintextEncoding(std::initializer_list<uint32_t> arr) : std::vector<uint32_t>(arr) {}
+
+	PackedIntPlaintextEncoding() : std::vector<uint32_t>() {}
+
+	/** Interface for the operation of converting from current plaintext encoding to ilVectorArray2n.
+	*
+	* @param  modulus - used for encoding.
+	* @param  *ilVectorArray2n encoded plaintext - output argument.
+	*/
+	void Encode(const BigBinaryInteger &modulus, ILVectorArray2n *iLVectorArray2n, size_t start_from=0, size_t length=0) const;
+
+	/** Interface for the operation of converting from ILVector2n to current plaintext encoding.
+	*
+	* @param  modulus - used for encoding.
+	* @param  *ilVectorArray2n encoded plaintext - input argument.
+	*/
+	void Decode(const BigBinaryInteger &modulus,  ILVectorArray2n *iLVectorArray2n);
+
+	/** Interface for the operation of converting from current plaintext encoding to ILVector2n.
+	*
+	* @param  modulus - used for encoding.
+	* @param  *ilVector encoded plaintext - output argument.
+	*/
+	void Encode(const BigBinaryInteger &modulus, ILVector2n *ilVector, size_t start_from=0, size_t length=0) const;
+
+	/** Interface for the operation of converting from ILVector2n to current plaintext encoding.
+	*
+	* @param  modulus - used for encoding.
+	* @param  *ilVector encoded plaintext - input argument.
+	*/
+	void Decode(const BigBinaryInteger &modulus,  ILVector2n *ilVector);
+
+	void Unpad(const BigBinaryInteger &modulus);
+
+	size_t GetChunksize(const usint cyc, const BigBinaryInteger& ptm) const;
+
+	size_t GetLength() const { return this->size(); }
+
+	bool CompareTo(const Plaintext& other) const {
+		const std::vector<uint32_t>& lv = dynamic_cast<const std::vector<uint32_t>&>(*this);
+		const std::vector<uint32_t>& rv = dynamic_cast<const std::vector<uint32_t>&>(other);
+		return lv == rv;
+	}
+};
+
+}
+
+#endif
