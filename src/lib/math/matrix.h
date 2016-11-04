8fafa652282755bf8b8aec9bcb9391d249a9425d
Lisa Bahler <lbahler@appcomsci.com>
2016-11-04 12:19:25 -0400

    Include <string> and <typeinfo> for debugging.

--- src/lib/math/matrix.h
+++ src/lib/math/matrix.h
@@ -31,6 +31,8 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 #include <assert.h>
 #include <stdlib.h>
 
+#include <typeinfo>
+#include <string>
 #include <iostream>
 #include <functional>
 #include <math.h>
