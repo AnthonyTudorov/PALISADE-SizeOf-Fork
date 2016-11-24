d793d407e00100654d3090d10a0d11da920367df
GYANA <grs22@njit.edu>
2016-11-24 15:23:10 -0500

    benchmarking header include issue fixed

--- benchmark/include/libsrc/benchmark.cc
+++ benchmark/include/libsrc/benchmark.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "benchmark/benchmark.h"
+#include "../benchmark/benchmark.h"
 #include "internal_macros.h"
 
 #ifndef BENCHMARK_OS_WINDOWS
