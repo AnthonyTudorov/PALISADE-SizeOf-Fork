8276537d0802f827b731c0bf7ae2fd70ebff5b8b
Jerry Ryan <gwryan@gmail.com>
2017-02-22 11:42:05 -0500

    makefile cleanup. SHE demo fix

--- benchmark/src/SHE.cpp
+++ benchmark/src/SHE.cpp
@@ -22,12 +22,7 @@
 using namespace std;
 using namespace lbcrypto;
 
-string parms[] = { "Null", "Null2", "LTV5", /*"StSt6",*/ "FV2" };
-
-static void CustomArguments(benchmark::internal::Benchmark* b) {
-	  for (int i = 0; i < (sizeof(parms)/sizeof(parms[0])); ++i)
-		  b->Arg(i);
-	}
+string parms[] = { "Null", "Null2", "LTV5", "FV1", "FV2" };
 
 static std::vector<uint32_t> makeVector(int siz, int ptmi) {
 	std::vector<uint32_t>			elem;
@@ -38,18 +33,7 @@ static std::vector<uint32_t> makeVector(int siz, int ptmi) {
 	return std::move(elem);
 }
 
-static void doEvalAdd(CryptoContext<ILVector2n>& cc, shared_ptr<Ciphertext<ILVector2n>> ct1, shared_ptr<Ciphertext<ILVector2n>> ct2) {
-	shared_ptr<Ciphertext<ILVector2n>> ctP = cc.EvalAdd(ct1, ct2);
-}
-
-static void doEvalMult(CryptoContext<ILVector2n>& cc, shared_ptr<Ciphertext<ILVector2n>> ct1, shared_ptr<Ciphertext<ILVector2n>> ct2) {
-	shared_ptr<Ciphertext<ILVector2n>> ctP = cc.EvalMult(ct1, ct2);
-}
-
-typedef void (*SHEfp)(CryptoContext<ILVector2n>& cc, shared_ptr<Ciphertext<ILVector2n>> ct1, shared_ptr<Ciphertext<ILVector2n>> ct2);
-
-
-static void eval_LATTICE(CryptoContext<ILVector2n>& cc, SHEfp f) {	// function
+static void setup_SHE(CryptoContext<ILVector2n>& cc, shared_ptr<Ciphertext<ILVector2n>>& ct1, shared_ptr<Ciphertext<ILVector2n>>& ct2) {
 	int nel = cc.GetElementParams()->GetCyclotomicOrder()/2;
 	const BigBinaryInteger& ptm = cc.GetCryptoParameters()->GetPlaintextModulus();
 	uint32_t ptmi = ptm.ConvertToInt();
@@ -59,27 +43,29 @@ static void eval_LATTICE(CryptoContext<ILVector2n>& cc, SHEfp f) {	// function
 	IntPlaintextEncoding p1( makeVector(nel, ptmi) );
 	IntPlaintextEncoding p2( makeVector(nel, ptmi) );
 
-	vector<shared_ptr<Ciphertext<ILVector2n>>> ct1 = cc.Encrypt(kp.publicKey, p1, false);
-	vector<shared_ptr<Ciphertext<ILVector2n>>> ct2 = cc.Encrypt(kp.publicKey, p2, false);
+	vector<shared_ptr<Ciphertext<ILVector2n>>> ct1V = cc.Encrypt(kp.publicKey, p1, false);
+	vector<shared_ptr<Ciphertext<ILVector2n>>> ct2V = cc.Encrypt(kp.publicKey, p2, false);
 
 	cc.EvalMultKeyGen(kp.secretKey);
 
-	(*f)(cc, ct1[0], ct2[0]);
+	ct1 = ct1V[0];
+	ct2 = ct2V[0];
 }
 
-void BM_evalAdd_LATTICE(benchmark::State& state) { // benchmark
+void BM_evalAdd_SHE(benchmark::State& state) { // benchmark
 	CryptoContext<ILVector2n> cc;
+	shared_ptr<Ciphertext<ILVector2n>> ct1, ct2;
 
 	if( state.thread_index == 0 ) {
 		cc = CryptoContextHelper<ILVector2n>::getNewContext(parms[state.range(0)]);
 		cc.Enable(ENCRYPTION);
 		cc.Enable(SHE);
+
+		setup_SHE(cc, ct1, ct2);
 	}
 
 	while (state.KeepRunning()) {
-		eval_LATTICE(cc, doEvalAdd);		// note even with -O3 it appears
-		// this is not optimized out
-		// though check with your compiler
+		shared_ptr<Ciphertext<ILVector2n>> ctP = cc.EvalAdd(ct1, ct2);
 	}
 
 //	ChineseRemainderTransformFTT::GetInstance().Destroy();
@@ -87,29 +73,34 @@ void BM_evalAdd_LATTICE(benchmark::State& state) { // benchmark
 //	ILVector2n::DestroyPreComputedSamples();
 }
 
-BENCHMARK(BM_evalAdd_LATTICE)->Apply(CustomArguments);;
+BENCHMARK(BM_evalAdd_SHE)->ArgName(parms[0])->Arg(0);
+BENCHMARK(BM_evalAdd_SHE)->ArgName(parms[1])->Arg(1);
+BENCHMARK(BM_evalAdd_SHE)->ArgName(parms[2])->Arg(2);
+BENCHMARK(BM_evalAdd_SHE)->ArgName(parms[3])->Arg(3);
+BENCHMARK(BM_evalAdd_SHE)->ArgName(parms[4])->Arg(4);
 
-void BM_evaMult_LATTICE(benchmark::State& state) { // benchmark
+void BM_evalMult_SHE(benchmark::State& state) { // benchmark
 	CryptoContext<ILVector2n> cc;
+	shared_ptr<Ciphertext<ILVector2n>> ct1, ct2;
 
 	if( state.thread_index == 0 ) {
 		cc = CryptoContextHelper<ILVector2n>::getNewContext(parms[state.range(0)]);
 		cc.Enable(ENCRYPTION);
 		cc.Enable(SHE);
+
+		setup_SHE(cc, ct1, ct2);
 	}
 
 	while (state.KeepRunning()) {
-		eval_LATTICE(cc, doEvalMult);		// note even with -O3 it appears
-		// this is not optimized out
-		// though check with your compiler
+		shared_ptr<Ciphertext<ILVector2n>> ctP = cc.EvalMult(ct1, ct2);
 	}
-
-//	ChineseRemainderTransformFTT::GetInstance().Destroy();
-//	NumberTheoreticTransform::GetInstance().Destroy();
-//	ILVector2n::DestroyPreComputedSamples();
 }
 
-BENCHMARK(BM_evaMult_LATTICE)->Apply(CustomArguments);
+BENCHMARK(BM_evalMult_SHE)->ArgName(parms[0])->Arg(0);
+BENCHMARK(BM_evalMult_SHE)->ArgName(parms[1])->Arg(1);
+BENCHMARK(BM_evalMult_SHE)->ArgName(parms[2])->Arg(2);
+BENCHMARK(BM_evalMult_SHE)->ArgName(parms[3])->Arg(3);
+BENCHMARK(BM_evalMult_SHE)->ArgName(parms[4])->Arg(4);
 
 //execute the benchmarks
 BENCHMARK_MAIN()
