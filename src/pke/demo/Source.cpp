90d735f1c8b3d38352da796fe0049384d0a3a2c3
Gerard Ryan <gwryan@njit.edu>
2017-03-03 10:37:34 -0500

    small tweak before

--- src/pke/demo/Source.cpp
+++ src/pke/demo/Source.cpp
@@ -36,7 +36,7 @@ Redistribution and use in source and binary forms, with or without modification,
 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-*/
+ */
 
 #include <iostream>
 #include <fstream>
@@ -86,10 +86,12 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	auto parmfind = CryptoContextParameterSets.find(parmSetName);
-	if( parmfind == CryptoContextParameterSets.end() ) {
-		cout << "Parameter set " << parmSetName << " is not a recognized name" << endl;
-		haveName = false;
+	if( haveName ) {
+		auto parmfind = CryptoContextParameterSets.find(parmSetName);
+		if( parmfind == CryptoContextParameterSets.end() ) {
+			cout << "Parameter set " << parmSetName << " is not a recognized name" << endl;
+			haveName = false;
+		}
 	}
 
 	if( !haveName ) {
@@ -110,7 +112,7 @@ int main(int argc, char *argv[])
 	if( beVerbose ) cout << "Initializing crypto system" << endl;
 
 	CryptoContext<ILVector2n> cc = CryptoContextHelper<ILVector2n>::getNewContext(parmSetName);
-	
+
 	// enable features
 	cc.Enable(ENCRYPTION);
 	cc.Enable(PRE);
