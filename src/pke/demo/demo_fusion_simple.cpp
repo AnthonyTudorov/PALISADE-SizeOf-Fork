b0eacf9b22ac0bf882b8067834f643d18fd23e63
Kurt Rohloff <rohloff@njit.edu>
2017-05-23 17:45:51 -0400

    3-party addition is working.

--- src/pke/demo/demo_fusion_simple.cpp
+++ src/pke/demo/demo_fusion_simple.cpp
@@ -156,11 +156,13 @@ int main(int argc, char *argv[]) {
 	// Initialize Public Key Containers
 	LPKeyPair<ILVector2n> kp1;
 	LPKeyPair<ILVector2n> kp2;
+	LPKeyPair<ILVector2n> kp3;
 
 	LPKeyPair<ILVector2n> kpFusion;
 
 	shared_ptr<LPEvalKey<ILVector2n>> evalKey1;
 	shared_ptr<LPEvalKey<ILVector2n>> evalKey2;
+	shared_ptr<LPEvalKey<ILVector2n>> evalKey3;
 	
 	////////////////////////////////////////////////////////////
 	// Perform Key Generation Operation
@@ -172,6 +174,7 @@ int main(int argc, char *argv[]) {
 
 	kp1 = cc.KeyGen();
 	kp2 = cc.FusionKeyGen(kp1.publicKey);
+	kp3 = cc.FusionKeyGen(kp1.publicKey);
 
 	finish = currentDateTime();
 	diff = finish - start;
@@ -181,8 +184,11 @@ int main(int argc, char *argv[]) {
 		std::cout << "Key generation failed!" << std::endl;
 		exit(1);
 	}
-
 	if( !kp2.good() ) {
+		std::cout << "Key generation failed!" << std::endl;
+		exit(1);
+	}
+	if( !kp3.good() ) {
 		std::cout << "Key generation failed!" << std::endl;
 		exit(1);
 	}
@@ -203,6 +209,7 @@ int main(int argc, char *argv[]) {
 	vector<shared_ptr<LPPrivateKey<ILVector2n>>> secretKeys;
 	secretKeys.push_back(kp1.secretKey);
 	secretKeys.push_back(kp2.secretKey);
+	secretKeys.push_back(kp3.secretKey);
 
 	kpFusion = cc.FusionKeyGen(secretKeys);	// This is the same core key generation operation.
 
@@ -230,6 +237,7 @@ int main(int argc, char *argv[]) {
 
 	evalKey1 = cc.ReKeyGen(kpFusion.secretKey, kp1.secretKey);
 	evalKey2 = cc.ReKeyGen(kpFusion.secretKey, kp2.secretKey);
+	evalKey3 = cc.ReKeyGen(kpFusion.secretKey, kp3.secretKey);
 
 	finish = currentDateTime();
 	diff = finish - start;
@@ -245,8 +253,10 @@ int main(int argc, char *argv[]) {
 
 	std::vector<uint32_t> vectorOfInts1 = {2,2,2,2,2,2,0,0,0,0,0,0};
 	std::vector<uint32_t> vectorOfInts2 = {3,3,3,3,3,0,0,0,0,0,0,0};
+	std::vector<uint32_t> vectorOfInts3 = {1,1,1,1,0,0,0,0,0,0,0,0};
 	IntPlaintextEncoding plaintext1(vectorOfInts1);
 	IntPlaintextEncoding plaintext2(vectorOfInts2);
+	IntPlaintextEncoding plaintext3(vectorOfInts3);
 
 	//std::vector<uint32_t> vectorOfIntsAdd = { 2, 1, 1, 3, 0, 0, 0, 0, 3, 0, 3, 3, 3, 3 };
 	//IntPlaintextEncoding plaintextAdd(vectorOfIntsAdd);
@@ -255,16 +265,15 @@ int main(int argc, char *argv[]) {
 	// Encryption
 	////////////////////////////////////////////////////////////
 
-	std::vector<vector<shared_ptr<Ciphertext<ILVector2n>>>> numeratorCiphertextInt(100); // Defaults to zero initial value
-	std::vector<vector<shared_ptr<Ciphertext<ILVector2n>>>> denominatorCiphertextInt(100); // Defaults to zero initial value
-
 	start = currentDateTime();
 
 	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertext1;
 	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertext2;
+	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertext3;
 
 	ciphertext1 = cc.Encrypt(kp1.publicKey, plaintext1, true);
 	ciphertext2 = cc.Encrypt(kp2.publicKey, plaintext2, true);
+	ciphertext3 = cc.Encrypt(kp3.publicKey, plaintext3, true);
 	
 	finish = currentDateTime();
 	diff = finish - start;
@@ -282,9 +291,11 @@ int main(int argc, char *argv[]) {
 
 	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertext1New;
 	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertext2New;
+	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertext3New;
 
 	ciphertext1New = cc.ReEncrypt(evalKey1, ciphertext1);
 	ciphertext2New = cc.ReEncrypt(evalKey2, ciphertext2);
+	ciphertext3New = cc.ReEncrypt(evalKey3, ciphertext3);
 
 	finish = currentDateTime();
 	diff = finish - start;
@@ -297,14 +308,17 @@ int main(int argc, char *argv[]) {
 	// EvalAdd Operation on Re-Encrypted Data
 	////////////////////////////////////////////////////////////
 
+	shared_ptr<Ciphertext<ILVector2n>> ciphertextAddNew12;
+	shared_ptr<Ciphertext<ILVector2n>> ciphertextAddNew123;
+
 	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertextAddVectNew;
-	shared_ptr<Ciphertext<ILVector2n>> ciphertextAddNew;
 
 	start = currentDateTime();
 
-	ciphertextAddNew = cc.EvalAdd(ciphertext1New[0],ciphertext2New[0]);
+	ciphertextAddNew12 = cc.EvalAdd(ciphertext1New[0],ciphertext2New[0]);
+	ciphertextAddNew123 = cc.EvalAdd(ciphertextAddNew12,ciphertext3New[0]);
 
-	ciphertextAddVectNew.push_back(ciphertextAddNew);
+	ciphertextAddVectNew.push_back(ciphertextAddNew123);
 
 	finish = currentDateTime();
 	diff = finish - start;
@@ -333,6 +347,7 @@ int main(int argc, char *argv[]) {
 	cout << "\n Original Plaintext: \n";
 	cout << plaintext1 << endl;
 	cout << plaintext2 << endl;
+	cout << plaintext3 << endl;
 
 	cout << "\n Resulting Added Plaintext with Re-Encryption: \n";
 	cout << plaintextAddNew << endl;
@@ -345,13 +360,16 @@ int main(int argc, char *argv[]) {
 
 	IntPlaintextEncoding plaintextAddNew1;
 	IntPlaintextEncoding plaintextAddNew2;
+	IntPlaintextEncoding plaintextAddNew3;
 
 	ILVector2n partialPlaintext1;
 	ILVector2n partialPlaintext2;
+	ILVector2n partialPlaintext3;
 	//IntPlaintextEncoding plaintextAddNewFinal;
 
 	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertextPartial1;
 	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertextPartial2;
+	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertextPartial3;
 
 	IntPlaintextEncoding plaintextFusionNew;
 
@@ -364,10 +382,12 @@ int main(int argc, char *argv[]) {
 
 	ciphertextPartial1 = cc.FusionDecryptMaster(kp1.secretKey, ciphertextAddVectNew);
 	ciphertextPartial2 = cc.FusionDecryptMain(kp2.secretKey, ciphertextAddVectNew);
+	ciphertextPartial3 = cc.FusionDecryptMain(kp3.secretKey, ciphertextAddVectNew);
 
 	vector<vector<shared_ptr<Ciphertext<ILVector2n>>>> partialCiphertextVec;
 	partialCiphertextVec.push_back(ciphertextPartial1);
 	partialCiphertextVec.push_back(ciphertextPartial2);
+	partialCiphertextVec.push_back(ciphertextPartial3);
 
 	DecryptResult resultFusion = cc.FusionDecrypt(partialCiphertextVec, &plaintextFusionNew, true);
 //	DecryptResult resultFusion = cc.FusionDecrypt(ciphertextPartial1, ciphertextPartial2, &plaintextFusionNew, true);
@@ -380,6 +400,7 @@ int main(int argc, char *argv[]) {
 	cout << "\n Original Plaintext: \n";
 	cout << plaintext1 << endl;
 	cout << plaintext2 << endl;
+	cout << plaintext3 << endl;
 
 	plaintextFusionNew.resize(plaintext1.size());
 
