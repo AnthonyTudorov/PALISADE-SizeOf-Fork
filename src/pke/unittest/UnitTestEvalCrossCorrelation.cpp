0420deec60914cd922ff44a267b1fbbc66b10229
Gyana Sahu <grs22@njit.edu>
2017-06-12 13:02:21 -0400

    cross correlation and linear regression unit tests added

---
+++ src/pke/unittest/UnitTestEvalCrossCorrelation.cpp
@@ -0,0 +1,244 @@
+/*
+PRE SCHEME PROJECT, Crypto Lab, NJIT
+Version:
+v00.01
+Last Edited:
+12/22/2015 2:37PM
+List of Authors:
+TPOC:
+Dr. Kurt Rohloff, rohloff@njit.edu
+Programmers:
+Dr. Yuriy Polyakov, polyakov@njit.edu
+Gyana Sahu, grs22@njit.edu
+Nishanth Pasham, np386@njit.edu
+Description:
+This code tests the transform feature of the PALISADE lattice encryption library.
+
+License Information:
+
+Copyright (c) 2015, New Jersey Institute of Technology (NJIT)
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "include/gtest/gtest.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+#include "../lib/cryptocontext.h"
+
+#include "encoding/byteplaintextencoding.h"
+#include "encoding/intplaintextencoding.h"
+#include "encoding/packedintplaintextencoding.h"
+
+#include "utils/debug.h"
+
+using namespace std;
+using namespace lbcrypto;
+
+class UnitTestEvalCrossCorrelation : public ::testing::Test {
+protected:
+	virtual void SetUp() {}
+
+	virtual void TearDown() {}
+
+public:
+};
+
+usint BVCrossCorrelation();
+usint FVCrossCorrelation();
+
+
+TEST(UTEvalCC, Test_BV_EvalCC) {
+
+	usint result = BVCrossCorrelation();
+
+	EXPECT_EQ(result, 11);
+	
+}
+
+
+TEST(UTEvalCC, Test_FV_EvalCC) {
+	
+	usint result = FVCrossCorrelation();
+
+	EXPECT_EQ(result, 11);
+
+}
+
+usint BVCrossCorrelation() {
+
+	usint m = 22;
+	usint p = 89;
+	BigBinaryInteger modulusP(p);
+
+	BigBinaryInteger modulusQ("955263939794561");
+	BigBinaryInteger squareRootOfRoot("941018665059848");
+
+	BigBinaryInteger bigmodulus("80899135611688102162227204937217");
+	BigBinaryInteger bigroot("77936753846653065954043047918387");
+
+	auto cycloPoly = GetCyclotomicPolynomial<BigBinaryVector, BigBinaryInteger>(m, modulusQ);
+	ChineseRemainderTransformArb<BigBinaryInteger, BigBinaryVector>::GetInstance().SetCylotomicPolynomial(cycloPoly, modulusQ);
+
+	PackedIntPlaintextEncoding::SetParams(modulusP, m);
+
+	float stdDev = 4;
+
+	usint batchSize = 8;
+
+	shared_ptr<ILParams> params(new ILParams(m, modulusQ, squareRootOfRoot, bigmodulus, bigroot));
+
+	shared_ptr<EncodingParams> encodingParams(new EncodingParams(modulusP, PackedIntPlaintextEncoding::GetInitRoot(modulusP), batchSize));
+
+	CryptoContext<ILVector2n> cc = CryptoContextFactory<ILVector2n>::genCryptoContextBV(params, encodingParams, 8, stdDev);
+
+	cc.Enable(ENCRYPTION);
+	cc.Enable(SHE);
+
+	// Initialize the public key containers.
+	LPKeyPair<ILVector2n> kp = cc.KeyGen();
+
+	// Compute evaluation keys
+	cc.EvalSumKeyGen(kp.secretKey);
+	cc.EvalMultKeyGen(kp.secretKey);
+
+	auto zeroAlloc = [=]() { return lbcrypto::make_unique<PackedIntPlaintextEncoding>(); };
+
+	Matrix<PackedIntPlaintextEncoding> x = Matrix<PackedIntPlaintextEncoding>(zeroAlloc, 2, 1);
+
+	x(0, 0) = { 0, 1, 1, 1, 0, 1, 1, 1 };
+	x(1, 0) = { 1, 0, 1, 1, 0, 1, 1, 0 };
+
+	Matrix<PackedIntPlaintextEncoding> y = Matrix<PackedIntPlaintextEncoding>(zeroAlloc, 2, 1);
+
+	y(0, 0) = { 0, 1, 1, 1, 0, 1, 1, 1 };
+	y(1, 0) = { 1, 0, 1, 1, 0, 1, 1, 0 };
+
+	////////////////////////////////////////////////////////////
+	//Encryption
+	////////////////////////////////////////////////////////////
+
+	shared_ptr<Matrix<RationalCiphertext<ILVector2n>>> xEncrypted = cc.EncryptMatrix(kp.publicKey, x);
+
+	shared_ptr<Matrix<RationalCiphertext<ILVector2n>>> yEncrypted = cc.EncryptMatrix(kp.publicKey, y);
+
+
+	////////////////////////////////////////////////////////////
+	//Linear Regression
+	////////////////////////////////////////////////////////////
+
+	auto result = cc.EvalCrossCorrelation(xEncrypted, yEncrypted, batchSize);
+
+	////////////////////////////////////////////////////////////
+	//Decryption
+	////////////////////////////////////////////////////////////
+
+	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertextCC;
+
+	ciphertextCC.push_back(result);
+
+	PackedIntPlaintextEncoding intArrayNew;
+
+	cc.Decrypt(kp.secretKey, ciphertextCC, &intArrayNew, false);
+
+	return intArrayNew[0];
+}
+
+
+usint FVCrossCorrelation() {
+
+	usint m = 22;
+	usint p = 89; // we choose s.t. 2m|p-1 to leverage CRTArb
+	BigBinaryInteger modulusQ("72385066601");
+	BigBinaryInteger modulusP(p);
+	BigBinaryInteger rootOfUnity("69414828251");
+	BigBinaryInteger bigmodulus("77302754575416994210914689");
+	BigBinaryInteger bigroot("76686504597021638023705542");
+
+	auto cycloPoly = GetCyclotomicPolynomial<BigBinaryVector, BigBinaryInteger>(m, modulusQ);
+	ChineseRemainderTransformArb<BigBinaryInteger, BigBinaryVector>::SetCylotomicPolynomial(cycloPoly, modulusQ);
+
+	float stdDev = 4;
+
+	shared_ptr<ILParams> params(new ILParams(m, modulusQ, rootOfUnity, bigmodulus, bigroot));
+
+	BigBinaryInteger bigEvalMultModulus("37778931862957161710549");
+	BigBinaryInteger bigEvalMultRootOfUnity("7161758688665914206613");
+	BigBinaryInteger bigEvalMultModulusAlt("1461501637330902918203684832716283019655932547329");
+	BigBinaryInteger bigEvalMultRootOfUnityAlt("570268124029534407621996591794583635795426001824");
+
+	auto cycloPolyBig = GetCyclotomicPolynomial<BigBinaryVector, BigBinaryInteger>(m, bigEvalMultModulus);
+
+	ChineseRemainderTransformArb<BigBinaryInteger, BigBinaryVector>::SetCylotomicPolynomial(cycloPolyBig, bigEvalMultModulus);
+
+	PackedIntPlaintextEncoding::SetParams(modulusP, m);
+
+	usint batchSize = 8;
+
+	shared_ptr<EncodingParams> encodingParams(new EncodingParams(modulusP, PackedIntPlaintextEncoding::GetInitRoot(modulusP), batchSize));
+
+	BigBinaryInteger delta(modulusQ.DividedBy(modulusP));
+
+	CryptoContext<ILVector2n> cc = CryptoContextFactory<ILVector2n>::genCryptoContextFV(params, encodingParams, 1, stdDev, delta.ToString(), OPTIMIZED,
+		bigEvalMultModulus.ToString(), bigEvalMultRootOfUnity.ToString(), 1, 9, 1.006, bigEvalMultModulusAlt.ToString(), bigEvalMultRootOfUnityAlt.ToString());
+
+	cc.Enable(ENCRYPTION);
+	cc.Enable(SHE);
+
+	// Initialize the public key containers.
+	LPKeyPair<ILVector2n> kp = cc.KeyGen();
+
+	// Compute evaluation keys
+	cc.EvalSumKeyGen(kp.secretKey);
+	cc.EvalMultKeyGen(kp.secretKey);
+
+	auto zeroAlloc = [=]() { return lbcrypto::make_unique<PackedIntPlaintextEncoding>(); };
+
+	Matrix<PackedIntPlaintextEncoding> x = Matrix<PackedIntPlaintextEncoding>(zeroAlloc, 2, 1);
+
+	x(0, 0) = { 0, 1, 1, 1, 0, 1, 1, 1 };
+	x(1, 0) = { 1, 0, 1, 1, 0, 1, 1, 0 };
+
+	Matrix<PackedIntPlaintextEncoding> y = Matrix<PackedIntPlaintextEncoding>(zeroAlloc, 2, 1);
+
+	y(0, 0) = { 0, 1, 1, 1, 0, 1, 1, 1 };
+	y(1, 0) = { 1, 0, 1, 1, 0, 1, 1, 0 };
+
+	////////////////////////////////////////////////////////////
+	//Encryption
+	////////////////////////////////////////////////////////////
+
+	shared_ptr<Matrix<RationalCiphertext<ILVector2n>>> xEncrypted = cc.EncryptMatrix(kp.publicKey, x);
+
+	shared_ptr<Matrix<RationalCiphertext<ILVector2n>>> yEncrypted = cc.EncryptMatrix(kp.publicKey, y);
+
+
+	////////////////////////////////////////////////////////////
+	//Linear Regression
+	////////////////////////////////////////////////////////////
+
+	auto result = cc.EvalCrossCorrelation(xEncrypted, yEncrypted, batchSize);
+
+	////////////////////////////////////////////////////////////
+	//Decryption
+	////////////////////////////////////////////////////////////
+
+	vector<shared_ptr<Ciphertext<ILVector2n>>> ciphertextCC;
+
+	ciphertextCC.push_back(result);
+
+	PackedIntPlaintextEncoding intArrayNew;
+
+	cc.Decrypt(kp.secretKey, ciphertextCC, &intArrayNew, false);
+
+	return intArrayNew[0];
+
+}
+
+
