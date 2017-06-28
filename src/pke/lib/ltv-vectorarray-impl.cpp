65942514cf4e797406c86a643fd85ecccc61aa76
Gerard Ryan <gwryan@njit.edu>
2017-06-28 08:23:06 -0400

    mac compile

--- src/pke/lib/ltv-vectorarray-impl.cpp
+++ src/pke/lib/ltv-vectorarray-impl.cpp
@@ -31,7 +31,7 @@ namespace lbcrypto {
 template class LPCryptoParametersLTV<ILDCRT2n>;
 template class LPPublicKeyEncryptionSchemeLTV<ILDCRT2n>;
 template class LPAlgorithmLTV<ILDCRT2n>;
-//template class LPAlgorithmParamsGenLTV<ILDCRT2n>;
+template class LPAlgorithmParamsGenLTV<ILDCRT2n>;
 template class LPAlgorithmPRELTV<ILDCRT2n>;
 template class LPAlgorithmSHELTV<ILDCRT2n>;
 template class LPLeveledSHEAlgorithmLTV<ILDCRT2n>;
