cc0de0db423c68362a6fd8a7cc819af6a4f84229
Jerry Ryan <gwryan@njit.edu>
2017-06-28 16:37:34 -0400

    script to check for prereqs for build

--- configure.sh
+++ configure.sh
@@ -1,191 +1,88 @@
-echo command creates a makefile to be used for different platforms
+echo Welcome to PALISADE
+echo This tool checks to see if you can run palisade in your environment,
+echo and tells you what tools you may need
 
 KernelName=`uname -s`
 
-echo $OS
-echo $KernelName
+echo OS is $OS
+echo Kernel is $KernelName
+echo
 
-### must locate compiler, boost
-
+##### REQUIRED
+### Compiler suite that supports C++11
 if [ "$OS" = "Windows_NT" ]
 then
-	build=mingw64
-	BOOSTINCLUDE="-I C:/boost_1_60_0"
-	BOOSTLIB="-L C:/boost_1_60_0/stage/lib/"
-	OMPINCLUDE=""
-	CC="g++ # This is the main compiler"
-	CPPSTD="-std=gnu++11"
-	LIBCMD="-s -shared -Wl,--subsystem,windows "
-	LIBSUFFIX=".dll"
-	EXESUFFIX=".exe
-
-	MINGWREGEX="-Lc:/Mingw64/mingw64/opt/lib -lregex -lshlwapi
-
-	RDYNAMIC="
-
-	JNIBUILD=" -I "C:/Program Files/java/jdk1.8.0_91/include" -I "C:/Program Files/java/jdk1.8.0_91/include/win32"
-	JNILIB := PalisadeCryptoWrapper.dll
-
-	PYINCLUDE="-I C:/Mingw64/mingw64/opt/include/python2.7
-	LIBPYTHON="-L C:/Mingw64/mingw64/opt/bin -lpython2.7 
+	CC="g++ -std=gnu++11"
 elif [ "$KernelName" = "Linux" ]
 then
-	build=Linux
-## for linux
-
-##BOOSTINCLUDE := -I /afs/cad/linux/boost-1.60-sl6/include/
-BOOSTINCLUDE := 
-
-OMPINCLUDE := 
-
-CC := g++ # This is the main compiler
-CPPSTD := -std=gnu++11
-
-
-LIBSUFFIX := .so
-LIBCMD := -fPIC -shared -Wl,--export-dynamic
-
-RDYNAMIC := -rdynamic
-
-JNIBUILD :=  
-JNILIB := libPalisadeCryptoWrapper.so
-
-PYINCLUDE := -I/usr/include/python2.7
-LIBPYTHON := -lpython2.7 
+	CC="g++ -std=gnu++11"
 elif [ "$KernelName" = "Darwin" ]
 then
-	build=MacOS
-## for mac OSx
-
-BOOSTINCLUDE := -I /boost_1_61_0
-BOOSTLIB := -L /boost_1_61_0/stage/lib/
-OMPINCLUDE := -I /opt/local/include/libomp -fopenmp
-#CPPSTD := -std=gnu++0x
-CC := clang++ # This is the main compiler
-CPPSTD := -std=c++11 -stdlib=libc++
-
-LIBSUFFIX := .dylib
-LIBCMD := -fopenmp -dynamiclib
-#-undefined suppress -flat_namespace
-
-RDYNAMIC := 
-
-JNIBUILD :=  
-JNILIB := libPalisadeCryptoWrapper.jnilib
-JNILINKPARM := -framework JavaVM
-
-PYINCLUDE := $(shell python-config --include)
-LIBPYTHON := -lpython2.7 
-elif [ "$KernelName" = "CYGWIN_NT" }
-then
-	build=cyg
+	CC="g++ -std=gnu++11"
+else
+	echo $OS and $KernelName are not supported
+	exit 1
 fi
 
-exit
+$CC -v >/dev/null 2>&1
+[ $? -ne 0 ] && echo Compiler is not available && exit 1
 
-else
+($CC -x c++ -o __test - <<XXX--end
+#if __cplusplus < 201103L
+#error This library requires a C++11 compliant compiler
+#endif
+int main() { return 0; }
+XXX--end
+) && __test && rm -f __test
 
-    ifeq ($(UNAME_S),Darwin)
-        #CCFLAGS += -D OSX
-	include Makefile.mac
-    endif
+[ $? -ne 0 ] && echo Compiler does not support C++11 && exit 1
 
-    ifeq ($(UNAME_S),CYGWIN_NT-6.1)
-        #CCFLAGS += -D CYGWIN
-	include Makefile.mingw
-    endif
+echo Compiler OK
 
-  #  UNAME_P := $(shell uname -p)
-  #  ifeq ($(UNAME_P),x86_64)
-  #      CCFLAGS += -D AMD64
-  #  endif
-  #  ifneq ($(filter %86,$(UNAME_P)),)
-  #      CCFLAGS += -D IA32
-  #  endif
-  #  ifneq ($(filter arm%,$(UNAME_P)),)
-  #      CCFLAGS += -D ARM
-  #  endif
-endif
+### make
 
-include Makefile.common
+make -v >/dev/null 2>&1
 
-# NOTE select the appropriate set of CPPFLAGS 
-# most code is checked into git with the first line active
+[ $? -ne 0 ] && echo make is not installed && exit 1
 
-#CPPFLAGS += -Wall $(CPPSTD) -w -g ## undefine for single thread debug NO OPTIMIZATION operation
-#CPPFLAGS += -Wall -O3 $(CPPSTD) -w -g ## undefine for single thread debug operation
-#CPPFLAGS += -Wall -O3 $(CPPSTD) -w -g -fopenmp  ##undefine for parallel debug operation
-#CPPFLAGS += -Wall -O3 $(CPPSTD) -w  -DNDEBUG  -pg ##undefine for single thread best performance operation with gprof profiling 
+echo make is installed
 
+### flex
 
-#main best performance configuration for parallel operation - cross-platform
-CPPFLAGS += -Wall -O3 $(CPPSTD) -w -fopenmp ##undefine for parallel best performance operation with debug
-#CPPFLAGS += -Wall -O3 $(CPPSTD) -w -fopenmp -m64 -DNDEBUG  ##undefine for parallel best performance operation with no debug
+flex --version >/dev/null 2>&1
 
-#THE OPTIONS BELOW SHOULD BE USED WITH CARE AS THEY USE A LOT OF AGGRESSIVE OPTIMIZATION OPTIONS
-#CPPFLAGS += -Wall $(CPPSTD) -w  -DNDEBUG -m64 -Ofast -flto -march=native -funroll-loops ##undefine for single thread best performance operation
-#CPPFLAGS += -Wall $(CPPSTD) -w  -DNDEBUG -m64 -Ofast -flto -march=native -funroll-loops -fopenmp ##undefine for multithread really best performance operation
-#CPPFLAGS += -Wall $(CPPSTD) -w  -m64 -Ofast -flto -march=native -funroll-loops -fopenmp -DNDEBUG ##undefine for multithread really best performance operation
-#CPPFLAGS += -Wall $(CPPSTD) -w  -m64 -Ofast -flto -march=native -funroll-loops -fopenmp ##undefine for multithread really best performance operation
+[ $? -ne 0 ] && echo flex is not installed && exit 1
 
+echo flex is installed
 
-TEST_LIB := -pthread -lgomp 
+### bison
 
+bison --version >/dev/null 2>&1
 
-#build directory
-BUILDDIR := build
+[ $? -ne 0 ] && echo bison is not installed && exit 1
 
-#bin directory
-BINDIR := bin
+echo bison is installed
 
-#sources for palisade library
-SRCLIBDIR := src/lib
-EXTLIBDIR := bin/lib
-EXTTESTDIR := bin/unittest
-EXTDEMODIR := bin/demo
+### Open MP
 
-# extentions for source and header files
-SRCEXT := cpp
-HDREXT := h
+($CC -x c++ -o __test -fopenmp - <<XXX--end
+#include <omp.h>
+int main() {
+#pragma omp parallel
+{
+int t = omp_get_thread_num();
+}
+return 0;
+}
+XXX--end
+) && __test && rm -f __test
 
-$(objects) : %.o : %.cpp
+[ $? -ne 0 ] && echo Environment does not support OpenMP && exit 1
 
+echo OpenMP OK
 
-EXTLIB := -L$(EXTLIBDIR) -lpthread -lgomp -pg #-lmongoclient -L lib -lboost_thread-mt -lboost_filesystem-mt -lboost_system-mt ## include profiling
-INC := -I src/core/lib -I src/pke/lib -I src/trapdoor/lib -I test $(OMPINCLUDE) $(BOOSTINCLUDE)
+### 128 bit unsigned int if you want native
 
+	MINGWREGEX="-Lc:/Mingw64/mingw64/opt/lib -lregex -lshlwapi"
 
-#the name of the shared object library for palisade objects.  
-PALISADECORELIB := libPALISADEcore$(LIBSUFFIX)
-PALISADEPKELIB := libPALISADEpke$(LIBSUFFIX)
-PALISADETRAPDOORLIB := libPALISADEtrapdoor$(LIBSUFFIX)
-PALISADEPYLIB := pycrypto$(LIBSUFFIX)
-
-# run make for all components. you can run any individual component separately
-#  by invoking   "make alltargets"  for example
-# each corresponding makefile will make the allxxxx target
-all: allcore allpke alltrapdoor apidocs 
-
-alljava: allcore allpke jwrapper
-
-allpython: allcore allpke alltrapdoor pywrapper
-
-alldemos: allcoredemos allpkedemos alltrapdoordemos
-
-testall: testcore testpke testtrapdoor
-
-# clean up all components. you can clean any individual compoenent separately
-#  by invoking   "make cleantargets"  for example
-# each corresponding makefile will make the cleanxxxx target
-.PHONEY: clean
-clean: cleancore cleanpke cleantrapdoor cleandocs 
-	@echo 'Cleaning top level autogenerated directories'
-	$(RM) -rf bin build
-
-include Makefile.core
-include Makefile.pke
-include Makefile.trapdoor
-include Makefile.wrapper
-include Makefile.benchmark     #builds the benchmark framework
-include Makefile.docs          #populates doxgen documentation
-
+	OMPINCLUDE="-I /opt/local/include/libomp -fopenmp"
