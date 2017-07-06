af59ad5cd2e08c1f89b55e7e226e2182b9d678f5
Gerard Ryan <gwryan@njit.edu>
2017-07-06 09:09:39 -0400

    first pass at ElemParamFactory

---
+++ src/core/lib/lattice/elemparamfactory.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file plaintext.h Represents and defines plaintext objects in Palisade.
+ * @author  TPOC: palisade@njit.edu
+ *
+ * @copyright Copyright (c) 2017, New Jersey Institute of Technology (NJIT)
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include "elemparamfactory.h"
+
+namespace lbcrypto {
+
+struct ElemParamFactory::ElemParmSet DefaultSet[] = {
+			{ 1024,
+					BigBinaryInteger("34359724033"),
+					BigBinaryInteger("11574709249") },
+			{ 2048,
+					BigBinaryInteger("1152921504606830593"),
+					BigBinaryInteger("512911795914445712") },
+			{ 4096,
+					BigBinaryInteger("83076749736557242056487941267259393"),
+					BigBinaryInteger("7174246090155309681020695614225666") },
+			{ 8192,
+					BigBinaryInteger("107839786668602559178668060348078522694548577690162289924414373888001"),
+					BigBinaryInteger("19714040362369820282800943210624474894547843835164146989800867233414") },
+			{ 16384,
+					BigBinaryInteger("88725430211866075506509253892578678509965986412026130405455346579667881849780019937279180995332466499116518750764914298518583115777"),
+					0 },
+	};
+
+} /* namespace lbcrypto */
