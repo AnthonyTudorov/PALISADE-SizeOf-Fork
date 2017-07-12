053e8255b07b5ad8e041cc5de59cc00007a284a3
Yuriy Polyakov <polyakov@njit.edu>
2017-07-11 20:42:54 -0400

    full demo is running; result not correct yet

--- src/pke/lib/cryptocontext.h
+++ src/pke/lib/cryptocontext.h
@@ -500,7 +500,6 @@ public:
 	 * @return the key to use
 	 */
 	const shared_ptr<LPEvalKey<Element>> GetEvalMultKey() const {
-		std::cout << "size = " << evalMultKeys.size() << std::endl;
 		if( evalMultKeys.size() != 1 )
 			throw std::logic_error("You need to use EvalMultKeyGen so that you have an EvalMultKey available");
 		return evalMultKeys[0];
@@ -511,9 +510,9 @@ public:
 	 * FIXME should be private?
 	 * @param evalMultKeys - new key map
 	 */
-	void SetEvalMultKeys(vector<shared_ptr<LPEvalKey<Element>>>& evalMultKeys) {
+	void SetEvalMultKeys(vector<shared_ptr<LPEvalKey<Element>>>& evalMultKeysNew) {
 		evalMultKeys.clear();
-		this->evalMultKeys = evalMultKeys;
+		this->evalMultKeys = evalMultKeysNew;
 	}
 
 	/**
