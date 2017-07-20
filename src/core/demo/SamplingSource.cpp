63d4b0072a140c9c3cf0c289aa729bb6bd8f9f78
Yuriy Polyakov <polyakov@njit.edu>
2017-07-20 14:07:57 -0400

    minor linux-specific fix

--- src/core/demo/SamplingSource.cpp
+++ src/core/demo/SamplingSource.cpp
@@ -59,7 +59,7 @@ int main() {
 	std::cout << "Sampling 100000 integers (Peikert): " << finish - start << " ms\n";
 
 	start = currentDateTime();
-	for (int i = 0;i < count;i++) {
+	for (size_t i = 0;i < count;i++) {
 		dgg.GenerateIntegerKarney(0, std);
 	}
 	finish = currentDateTime();
