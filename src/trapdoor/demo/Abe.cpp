e4faf70685bc02162fbd8bdacabd0080fa07bb6d
Hadi Sajjadpour <ss2959@njit.edu>
2017-08-07 10:59:50 -0400

    Better Experiments

--- src/trapdoor/demo/Abe.cpp
+++ src/trapdoor/demo/Abe.cpp
@@ -12,25 +12,72 @@ using namespace lbcrypto;
 
 int IBE_Test(int iter, int32_t base, usint ringDimension, usint k, bool offline);
 int TestKeyGenCP(const shared_ptr<ILParams> ilParams, usint m, usint ell, const usint s[], const RingMat &a, const RingMat &pubElemBPos, const RingMat &pubElemBNeg, const Poly &pubElemU, RingMat &sk);
-int CPABE_Test(usint iter, bool offline);
+int CPABE_Test(int iter, int32_t base, usint ringDimension, usint k, usint ell, BigInteger q, BigInteger rootOfUnity, bool offline);
+
+struct Params_Set {
+	usint base;			// Base
+	usint q;	        // modulus bit size
+	usint ringDimension;	
+	string modulus;
+	string rootOfUnity;
+};
 
 int main()
 {
 
 	
-	std::cout << "-------Start demo for CP-ABE-------" << std::endl;
-	CPABE_Test(1, true);
-	std::cout << "-------End demo for CP-ABE-------" << std::endl << std::endl;
+/*	std::cout << "-------Start demo for CP-ABE-------" << std::endl;
 
-/*	std::cout << "-------Start demo for IBE-------" << std::endl;
-	IBE_Test(10000, 16, 2048, 35, true); //iter. ring dimension, k, bool offline
-	std::cout << "-------End demo for IBE-------" << std::endl << std::endl;*/
+	Params_Set const cpabe_params[] = {
+		{ 2, 31, 1024, "2147577857", "1289872274"}, 
+		{ 4, 31, 1024, "2147577857", "663556699"},
+		{ 8, 31, 1024, "2147577857", "305569228"},
+		{ 16, 34, 2048, "17183748097", "15765111296"},
+		{ 32, 35, 2048, "34359754753", "11639358469"},
+		{ 64, 37, 2048, "68719484929", "13885715160"},
+		{ 128, 39, 2048, "137439072257", "122163940028"},
+		{ 256, 40, 2048, "274878136321", "206059740088"},
+		{ 512, 41, 2048, "549755904001", "300965887617"},
+		{ 1024, 44, 2048, "8796093050881", "3269718423574"}
+	};	
+
+	usint ell[] = { 6, 8, 16, 20, 32 }; 
+	for(usint i = 7; i < 10;i++){
+		BigInteger modulus(cpabe_params[i].modulus);
+		BigInteger rootOfUnity(cpabe_params[i].rootOfUnity);
+		for(usint j = 0; j < 5; j++){
+			CPABE_Test(10000, cpabe_params[i].base, cpabe_params[i].ringDimension, cpabe_params[i].q, ell[j], modulus, rootOfUnity, true);
+		}
+	}	
+	std::cout << "-------End demo for CP-ABE-------" << std::endl << std::endl;
+*/
+	std::cout << "-------Start demo for IBE-------" << std::endl;
+	Params_Set const ibe_params[] = {
+		{ 2, 31, 1024}, 
+		{ 4, 31, 1024},
+		{ 8, 33, 2048},
+		{ 16, 35, 2048},
+		{ 32, 38, 2048},
+		{ 64, 41, 2048},
+		{ 128, 43, 2048},
+		{ 256, 46, 2048},
+		{ 512, 48, 2048},
+		{ 1024, 49, 2048},
+		{ 2048, 51, 2048}
+	};	
+
+	for(usint i = 0; i < 10; i++){
+		IBE_Test(10, ibe_params[i].base, ibe_params[i].ringDimension, ibe_params[i].q, true); //iter. ring dimension, k, bool offline
+	}	
+
+	std::cout << "-------End demo for IBE-------" << std::endl << std::endl;
 
 	return 0;
 }
 
 int IBE_Test(int iter, int32_t base, usint ringDimension, usint k, bool offline)
 {
+
 	usint n = ringDimension*2;
 
 	BigInteger q = BigInteger::ONE << (k-1);
@@ -60,7 +107,7 @@ int IBE_Test(int iter, int32_t base, usint ringDimension, usint k, bool offline)
 	ChineseRemainderTransformFTT<BigInteger, BigVector>::GetInstance().PreCompute(rootOfUnity, n, q);
 
 	// for timing
-	long double start, finish, avg_keygen, avg_enc, avg_dec;
+	long double start, finish, avg_keygen_offline, avg_keygen_online, avg_enc, avg_dec;
 
 	IBE pkg, sender, receiver;
 
@@ -85,18 +132,23 @@ int IBE_Test(int iter, int32_t base, usint ringDimension, usint k, bool offline)
 	Poly ctC1(dug, ilParams, EVALUATION);
 
 	int failure = 0;
-	avg_keygen = avg_enc = avg_dec = 0.0;
+	avg_keygen_online = avg_keygen_offline = avg_enc = avg_dec = 0.0;
 
 	for(int i=0; i<iter; i++)
 	{
-		std::cout << "Iter no. " << i << std::endl;
+	//	std::cout << "Iter no. " << i << std::endl;
 
 		Poly u(dug, ilParams, EVALUATION);
 
 		shared_ptr<RingMat> perturbationVector;
 		
-		if(offline)
+		if(offline){
+			start = currentDateTime();
 			perturbationVector = pkg.KeyGenOffline(pubElemA.first, u, pubElemA.second, dgg);
+			finish = currentDateTime();
+			avg_keygen_offline += (finish - start);
+		}
+
 		start = currentDateTime();
 		
 		if(!offline)
@@ -105,8 +157,8 @@ int IBE_Test(int iter, int32_t base, usint ringDimension, usint k, bool offline)
 			pkg.KeyGenOnline(pubElemA.first, u, pubElemA.second, dgg, perturbationVector, &sk);
 		
 		finish = currentDateTime();
-		avg_keygen += (finish - start);
-		std::cout << "Key generation time : " << "\t" << (finish - start) << " ms" << std::endl;
+		avg_keygen_online += (finish - start);
+//		std::cout << "Key generation time : " << "\t" << (finish - start) << " ms" << std::endl;
 
 		// Encrypt a uniformly randomly selected message ptext (in ptext in $R_2$)
 		ptext.SetValues(bug.GenerateVector(ringDimension, q), COEFFICIENT);
@@ -117,26 +169,29 @@ int IBE_Test(int iter, int32_t base, usint ringDimension, usint k, bool offline)
 		sender.Encrypt(ilParams, pubElemA.first, u, ptext, dgg, dug, bug, &ctC0, &ctC1);
 		finish = currentDateTime();
 		avg_enc += (finish - start);
-		std::cout << "Encryption time : " << "\t" << (finish - start) << " ms" << std::endl;
+	//	std::cout << "Encryption time : " << "\t" << (finish - start) << " ms" << std::endl;
 
 		start = currentDateTime();
 		receiver.Decrypt(ilParams, sk, ctC0, ctC1, &dtext);
 		finish = currentDateTime();
 		avg_dec += (finish - start);
-		std::cout << "Decryption time : " << "\t" << (finish - start) << " ms" << std::endl;
+	//	std::cout << "Decryption time : " << "\t" << (finish - start) << " ms" << std::endl;
 
 		ptext.SwitchFormat();
 		if(ptext != dtext) {
 			failure++;
-			std::cout << "Encryption fails in iter no. " << i << " \n";
+		//	std::cout << "Encryption fails in iter no. " << i << " \n";
 			break;
 		}
 	}
 	if(failure == 0) {
 		std::cout << "Encryption/Decryption is successful after " << iter << " iterations!\n";
-		std::cout << "Average key generation time : " << "\t" << (avg_keygen)/iter << " ms" << std::endl;
+		std::cout << "Average key generation time online: " << "\t" << (avg_keygen_online)/iter << " ms" << std::endl;
+		std::cout << "Average key generation time offline: " << "\t" << (avg_keygen_offline)/iter << " ms" << std::endl;
+		std::cout << "Average key generation time total: " << "\t" << (avg_keygen_offline + avg_keygen_online)/iter << " ms" << std::endl;
 		std::cout << "Average encryption time : " << "\t" << (avg_enc)/iter << " ms" << std::endl;
 		std::cout << "Average decryption time : " << "\t" << (avg_dec)/iter << " ms" << std::endl;
+		std::cout << "----------------------------------------------------------------" << std::endl;
 	}
 
 	ChineseRemainderTransformFTT<BigInteger, BigVector>::GetInstance().Destroy();
@@ -144,18 +199,11 @@ int IBE_Test(int iter, int32_t base, usint ringDimension, usint k, bool offline)
 	return 0;
 }
 
-int CPABE_Test(usint iter, bool offline)
+int CPABE_Test(int iter, int32_t base, usint ringDimension, usint k, usint ell, BigInteger q, BigInteger rootOfUnity, bool offline)
 {
-	usint ringDimension = 1024;
+	
 	usint n = ringDimension*2;
-	usint k = 34;
-	int32_t base = 4;
-	usint ell = 32;
-
-	BigInteger q = BigInteger::ONE << (k-1);
-	q = lbcrypto::FirstPrime<BigInteger>(k,n);
-	BigInteger rootOfUnity(RootOfUnity(n, q));
-
+	
 	double val = q.ConvertToDouble();
 	double logTwo = log(val-1.0)/log(base)+1.0;
 	size_t k_ = (usint) floor(logTwo); /*+ 1;   (+1) is For NAF */
@@ -163,6 +211,7 @@ int CPABE_Test(usint iter, bool offline)
 	std::cout << "modulus length in base " << base << ": "<< k_ << std::endl;
 	std::cout << "root of unity: " << rootOfUnity << std::endl;
 	std::cout << "Standard deviation: " << SIGMA << std::endl;
+	std::cout << "ell: " << ell << std::endl;
 
 	usint m = k_+2;
 
@@ -183,7 +232,7 @@ int CPABE_Test(usint iter, bool offline)
 	Poly u(pubElemBPos(0,0));
 
 	// for timing
-	double start, finish, avg_keygen, avg_enc, avg_dec;
+	long double start, finish, avg_keygen_offline, avg_keygen_online, avg_enc, avg_dec;
 
 	CPABE pkg, sender, receiver;
 
@@ -212,8 +261,8 @@ int CPABE_Test(usint iter, bool offline)
 	Poly c1(dug, ilParams, EVALUATION);
 
 	int failure = 0;
-	avg_keygen = avg_enc = avg_dec = 0.0;
-	for(usint i=0; i<iter; i++)
+	avg_keygen_online = avg_keygen_offline = avg_enc = avg_dec = 0.0;
+	for(int i=0; i<iter; i++)
 	{
 //		std::cout << "Iter no. " << i << std::endl;
 
@@ -243,19 +292,22 @@ int CPABE_Test(usint iter, bool offline)
 		start = currentDateTime();
 
 		shared_ptr<RingMat> perturbationVector;
-
+		start = currentDateTime();
 		if(offline)
 			perturbationVector = pkg.KeyGenOffline( trapdoor.second, dgg);
-		start = currentDateTime();
+
+		finish = currentDateTime();
+		avg_keygen_offline += (finish - start);
 		
+		start = currentDateTime();		
 		if(offline)
 			pkg.KeyGenOnline(ilParams, s, trapdoor.first, pubElemBPos, pubElemBNeg, u, trapdoor.second, dgg, perturbationVector, &sk);
 		else
 			pkg.KeyGen(ilParams, s, trapdoor.first, pubElemBPos, pubElemBNeg, u, trapdoor.second, dgg, &sk);
 		
 		finish = currentDateTime();
-		avg_keygen += (finish - start);
-		std::cout << "Key generation time : " << "\t" << (finish - start) << " ms" << std::endl;
+		avg_keygen_online += (finish - start);
+//		std::cout << "Key generation time : " << "\t" << (finish - start) << " ms" << std::endl;
 		TestKeyGenCP(ilParams, m, ell, s, trapdoor.first, pubElemBPos, pubElemBNeg, u, sk);
 
 
@@ -271,13 +323,13 @@ int CPABE_Test(usint iter, bool offline)
 		sender.Encrypt(ilParams, trapdoor.first, pubElemBPos, pubElemBNeg, u, w, ptext, dgg, dug, bug, &ctW, &ctCPos, &nC, &c1);
 		finish = currentDateTime();
 		avg_enc += (finish - start);
-		std::cout << "Encryption time : " << "\t" << (finish - start) << " ms" << std::endl;
+	//	std::cout << "Encryption time : " << "\t" << (finish - start) << " ms" << std::endl;
 
 		start = currentDateTime();
 		receiver.Decrypt(ilParams, w, s, sk, ctW, ctCPos, nC, c1, &dtext);
 		finish = currentDateTime();
 		avg_dec += (finish - start);
-		std::cout << "Decryption time : " << "\t" << (finish - start) << " ms" << std::endl;
+	//	std::cout << "Decryption time : " << "\t" << (finish - start) << " ms" << std::endl;
 
 		ptext.SwitchFormat();
 		if(ptext != dtext) {
@@ -288,9 +340,12 @@ int CPABE_Test(usint iter, bool offline)
 	}
 	if(failure == 0) {
 		std::cout << "Encryption/Decryption is successful after " << iter << " iterations!\n";
-		std::cout << "Average key generation time : " << "\t" << (avg_keygen)/iter << " ms" << std::endl;
+		std::cout << "Average key generation time online: " << "\t" << (avg_keygen_online)/iter << " ms" << std::endl;
+		std::cout << "Average key generation time offline: " << "\t" << (avg_keygen_offline)/iter << " ms" << std::endl;
+		std::cout << "Average key generation time total: " << "\t" << (avg_keygen_offline + avg_keygen_online)/iter << " ms" << std::endl;
 		std::cout << "Average encryption time : " << "\t" << (avg_enc)/iter << " ms" << std::endl;
 		std::cout << "Average decryption time : " << "\t" << (avg_dec)/iter << " ms" << std::endl;
+		std::cout << "----------------------------------------------------------------" << std::endl;
 	}
 
 	delete[] w;
@@ -335,9 +390,9 @@ int TestKeyGenCP(
 
 	t1 += t2;
 
-	if (u == t1)
-		std::cout << "Key generation is successful!\n";
-	else
+	
+	//	std::cout << "Key generation is successful!\n";
+	if (!(u == t1))
 		std::cout << "Key generation fails!\n";
 	return 0;
 }
