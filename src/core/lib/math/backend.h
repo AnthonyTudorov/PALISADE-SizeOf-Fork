c60afa83210cba79c683e5fabea4429d8a003c78
Yuriy Polyakov <polyakov@njit.edu>
2017-10-05 22:45:47 -0400

    changed BE back to 2

--- src/core/lib/math/backend.h
+++ src/core/lib/math/backend.h
@@ -73,9 +73,9 @@
 //To select backend, please UNCOMMENT the appropriate line rather than changing the number on the
 //uncommented line (and breaking the documentation of the line)
 
-//#define MATHBACKEND 2
+#define MATHBACKEND 2
 //#define MATHBACKEND 4
-#define MATHBACKEND 6
+//#define MATHBACKEND 6
 //#define MATHBACKEND 7
 
 ////////// cpu_int code
