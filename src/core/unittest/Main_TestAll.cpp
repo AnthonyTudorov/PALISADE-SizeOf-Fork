97bfefccd1b48cb31f049b9304c201e286dc142c
Gerard Ryan <gwryan@njit.edu>
2017-10-27 10:58:39 -0400

    terse mode tests in core

--- src/core/unittest/Main_TestAll.cpp
+++ src/core/unittest/Main_TestAll.cpp
@@ -23,12 +23,12 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
- 
+
 #include <iostream>
 
 #include "../lib/lattice/dcrtpoly.h"
 #include "include/gtest/gtest.h"
-
+#include "include/gtest/gtest-all.cc"
 
 #include "math/backend.h"
 #include "utils/inttypes.h"
@@ -43,22 +43,97 @@
 
 using namespace std;
 using namespace lbcrypto;
+using namespace testing;
+
+class MinimalistPrinter : public EmptyTestEventListener {
+
+public:
+	void OnTestProgramStart(const ::testing::UnitTest& unit_test) {
+		cout << "Begin Test Run" << endl;
+		cout << GetMathBackendParameters() << endl;
+	}
+	void OnTestIterationStart(const ::testing::UnitTest& unit_test, int iteration) {}
+	void OnEnvironmentsSetUpStart(const ::testing::UnitTest& unit_test) {}
+	void OnEnvironmentsSetUpEnd(const ::testing::UnitTest& unit_test) {}
+	void OnTestCaseStart(const ::testing::TestCase& test_case) {}
+	void OnTestStart(const ::testing::TestInfo& test_info) {}
+
+	// Called after a failed assertion or a SUCCEED() invocation.
+	void OnTestPartResult(const ::testing::TestPartResult& test_part_result) {}
+
+	void OnTestEnd(const ::testing::TestInfo& test_info) {
+		if (test_info.result()->Passed() ) {
+			return;
+		}
+
+		auto tr = test_info.result();
+
+		for( int i=0; i < tr->total_part_count(); i++ ) {
+			auto pr = tr->GetTestPartResult(i);
+			if( pr.passed() )
+				continue;
+
+			internal::ColoredPrintf(internal::COLOR_GREEN,  "[ RUN      ] ");
+			printf("%s.%s\n", test_info.test_case_name(), test_info.name());
+			fflush(stdout);
+
+			auto n = pr.file_name();
+			if( n != NULL )
+				cout << n << ":" << pr.line_number() << "\n";
+
+			cout << pr.summary() << endl;
+
+			internal::ColoredPrintf(internal::COLOR_RED, "[  FAILED  ] ");
+			printf("%s.%s\n", test_info.test_case_name(), test_info.name());
+			fflush(stdout);
+			internal::PrintFullTestCommentIfPresent(test_info);
+		}
+	}
+	void OnTestCaseEnd(const ::testing::TestCase& test_case) {}
+	void OnEnvironmentsTearDownStart(const ::testing::UnitTest& unit_test) {}
+	void OnEnvironmentsTearDownEnd(const ::testing::UnitTest& /*unit_test*/) {}
+	void OnTestIterationEnd(const ::testing::UnitTest& unit_test, int iteration) {}
+
+	void OnTestProgramEnd(const ::testing::UnitTest& unit_test)  {
+		cout << "End Test Run of " << unit_test.test_to_run_count() << " cases, " << unit_test.successful_test_count() << " passed, " << unit_test.failed_test_count() << " failed" << endl;
+	}
+
+
+};
 
 int main(int argc, char **argv) {
 
-  ::testing::InitGoogleTest(&argc, argv);
-  
-  // if there are no filters used, default to omitting VERY_LONG tests
-  // otherwise we lose control over which tests we can run
-  //::testing::GTEST_FLAG(filter) = "*CRT_polynomial_multiplication_small";
+	::testing::InitGoogleTest(&argc, argv);
 
-  if (::testing::GTEST_FLAG(filter) == "*") {
-    ::testing::GTEST_FLAG(filter) = "-*_VERY_LONG";
-  }
+	bool terse=true;
+	for( int i = 1; i < argc; i++ ) {
+		if( string(argv[i]) == "-t" ) {
+			terse=true;
+		}
+	}
 
-  std::cout << GetMathBackendParameters() << std::endl;
-  int rv = RUN_ALL_TESTS();
+	// if there are no filters used, default to omitting VERY_LONG tests
+	// otherwise we lose control over which tests we can run
+	//::testing::GTEST_FLAG(filter) = "*CRT_polynomial_multiplication_small";
 
-  return rv;
+	if (::testing::GTEST_FLAG(filter) == "*") {
+		::testing::GTEST_FLAG(filter) = "-*_VERY_LONG";
+	}
+
+	::testing::TestEventListeners& listeners =
+			::testing::UnitTest::GetInstance()->listeners();
+
+	if( terse ) {
+		// Adds a listener to the end.  Google Test takes the ownership.
+		delete listeners.Release(listeners.default_result_printer());
+		listeners.Append(new MinimalistPrinter);
+	}
+	else {
+		std::cout << GetMathBackendParameters() << std::endl;
+	}
+
+	int rv = RUN_ALL_TESTS();
+
+	return rv;
 }
 
