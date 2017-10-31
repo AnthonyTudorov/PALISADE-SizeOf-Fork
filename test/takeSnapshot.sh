b71db8723de96ce8422be77895b8e3b330a84d5a
Jerry Ryan <gwryan@njit.edu>
2017-10-31 18:15:26 -0400

    makefile fixes for benchmarks

--- test/takeSnapshot.sh
+++ test/takeSnapshot.sh
@@ -1,32 +1,30 @@
 BMDIR=bin/benchmark
 # take benchmark snapshot
 
-if [ $# -ne 1 ]
-then
-	echo Usage is $0 filename-to-store-snapshot
-	exit 1
-fi
+###if [ $# -ne 1 ]
+###then
+	###echo Usage is $0 filename-to-store-snapshot
+	###exit 1
+###fi
 
-if [ -e $1 ]
-then
-	echo Sorry, that file already exists
-	exit 1
-fi
+###if [ -e $1 ]
+###then
+	###echo Sorry, that file already exists
+	###exit 1
+###fi
 
-smallbmargs="--benchmark_report_aggregates_only=true --benchmark_format=json"
-largebmargs="--benchmark_repetitions=20 --benchmark_report_aggregates_only=true --benchmark_format=json"
-
-echo "running, this will take a while..."
+smallbmargs="--benchmark_report_aggregates_only=true --benchmark_format=csv"
+largebmargs="--benchmark_repetitions=20 --benchmark_report_aggregates_only=true --benchmark_format=csv"
 
 for bm in BBIMath BBVMath NbTheory Lattice 
 do
 	echo $bm:
-	$BMDIR/${bm}* ${smallbmargs} >> $1
+	$BMDIR/${bm}* ${smallbmargs} 
 done
 
 for bm in Encoding Crypto SHE
 do
 	echo $bm:
-	$BMDIR/${bm}* ${largebmargs} >> $1
+	$BMDIR/${bm}* ${largebmargs} 
 done
 
