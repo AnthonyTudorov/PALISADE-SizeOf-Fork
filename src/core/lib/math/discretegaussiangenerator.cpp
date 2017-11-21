9b201a3a66561961e168f469de50ad13ea58e6ab
David Cousins <dcousins@bbn.com>
2017-11-21 13:50:49 -0500

    cleaned up at() in discretegaussiangenerator.cpp

--- src/core/lib/math/discretegaussiangenerator.cpp
+++ src/core/lib/math/discretegaussiangenerator.cpp
@@ -208,18 +208,10 @@ namespace lbcrypto {
 			sint v = (result.get())[i];
 			if (v < 0) {
 				v *= -1;
-#if 1 //MATHBACKEND != 6
 				ans.at(i)= modulus - IntType(v);
-#else
-				ans.atWithoutMod(i)= modulus - IntType(v);
-#endif
 			}
 			else {
-#if 1//MATHBACKEND != 6
 			  ans.at(i)= IntType(v);
-#else
-			  ans.atWithoutMod(i)= IntType(v);
-#endif
 			}
 		}
 
