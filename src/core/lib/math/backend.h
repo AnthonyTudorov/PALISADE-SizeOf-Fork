e7391444d5551e8598ad3eb496853108ca79fca5
Gerard Ryan <gwryan@njit.edu>
2017-11-29 18:44:32 -0500

    oops, should be 2

--- src/core/lib/math/backend.h
+++ src/core/lib/math/backend.h
@@ -69,9 +69,9 @@
 //uncommented line (and breaking the documentation of the line)
 
 #ifndef MATHBACKEND
-//#define MATHBACKEND 2
+#define MATHBACKEND 2
 //#define MATHBACKEND 4
-#define MATHBACKEND 6
+//#define MATHBACKEND 6
 //#define MATHBACKEND 7
 #endif
 
