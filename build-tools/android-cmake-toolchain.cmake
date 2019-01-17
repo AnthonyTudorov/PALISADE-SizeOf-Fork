e136c15322f15961662c26635cb37cc263e04266
Jerry Ryan <gwryan@njit.edu>
2019-01-17 14:38:55 -0500

    separate cross tools and cross tree for ntl

---
+++ build-tools/android-cmake-toolchain.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_SYSTEM_NAME Android)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CROSS_TRIPLE x86_64-linux-android CACHE STRING "cross compile target")
+set(tools /home/palisade/share-toolchain-x86_64)
+set(CMAKE_C_COMPILER ${tools}/bin/${CROSS_TRIPLE}-clang)
+set(CMAKE_CXX_COMPILER ${tools}/bin/${CROSS_TRIPLE}-clang++)
+set(CMAKE_LD ${tools}/bin/${CROSS_TRIPLE}-ld)
+set(CMAKE_AR ${tools}/bin/${CROSS_TRIPLE}-ar)
+set(CMAKE_RANLIB ${tools}/bin/${CROSS_TRIPLE}-ranlib)
+set(CMAKE_C_FLAGS "-fPIE -fPIC")
+###set(CMAKE_LD_FLAGS -pie)
+
+set(CMAKE_FIND_ROOT_PATH ${tools})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(NTLCROSS "HOST=${CROSS_TRIPLE} NATIVE=off CXX=${CMAKE_CXX_COMPILER} AR=${CMAKE_AR} RANLIB=${CMAKE_RANLIB}" CACHE STRING "force NTL cross compile")
