7a003621e9c4842f0918b9888fb232b9bb5ce568
Jerry Ryan <gwryan@njit.edu>
2019-01-21 00:10:24 -0500

    bld tools

--- build-tools/android-cmake-toolchain.cmake
+++ build-tools/android-cmake-toolchain.cmake
@@ -1,15 +1,15 @@
 set(CMAKE_SYSTEM_NAME Android)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_SYSTEM_PROCESSOR x86)
 
-set(CROSS_TRIPLE x86_64-linux-android CACHE STRING "cross compile target")
-set(tools /home/palisade/share-toolchain-x86_64)
-set(CMAKE_C_COMPILER ${tools}/bin/${CROSS_TRIPLE}-clang)
-set(CMAKE_CXX_COMPILER ${tools}/bin/${CROSS_TRIPLE}-clang++)
+set(CROSS_TRIPLE i686-linux-android CACHE STRING "cross compile target")
+set(tools /home/palisade/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64)
+set(CMAKE_C_COMPILER ${tools}/bin/${CROSS_TRIPLE}26-clang)
+set(CMAKE_CXX_COMPILER ${tools}/bin/${CROSS_TRIPLE}26-clang++)
 set(CMAKE_LD ${tools}/bin/${CROSS_TRIPLE}-ld)
 set(CMAKE_AR ${tools}/bin/${CROSS_TRIPLE}-ar)
 set(CMAKE_RANLIB ${tools}/bin/${CROSS_TRIPLE}-ranlib)
-set(CMAKE_C_FLAGS "-fPIE -fPIC")
-###set(CMAKE_LD_FLAGS -pie)
+set(CMAKE_C_FLAGS "-march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32 -fPIE -fPIC")
+set(CMAKE_LD_FLAGS -pie)
 
 set(CMAKE_FIND_ROOT_PATH ${tools})
 
--- src/wrappers/PALISADEjni/jni/Android.mk
+++ src/wrappers/PALISADEjni/jni/Android.mk
@@ -5,10 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_CPP_FEATURES := exceptions rtti
 
 ANDROID_STD := c++_shared
-LOCAL_CPPFLAGS += -fforce_enable_int128
 
 PBASE := ../../..
-THIRDP := ../../../../build/third-party/include
+THIRDP := ../../../../crossbuild/third-party/include
 RJ := ../../../../third-party/rapidjson/include
 LOCAL_C_INCLUDES := $(THIRDP) $(RJ) $(PBASE) $(PBASE)/core/lib
 
