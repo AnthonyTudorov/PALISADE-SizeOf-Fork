bfd4db93fe848a9e97c4c6f355202a373d16cf83
gwryan@njit.edu <gwryan@njit.edu>
2019-02-27 06:18:43 -0500

    unit tests as standalone executable on android

--- src/Android.mk
+++ src/Android.mk
@@ -70,21 +70,44 @@ include $(BUILD_SHARED_LIBRARY)
 LOCAL_PATH := $(JNI_PATH)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libPALISADEunit
+LOCAL_MODULE := libgtest
 
-UNIT_FILE_LIST := jni/unittest/testmain.cpp
+GOOGLETEST_ROOT = $(ANDROID_NDK)/sources/third_party/googletest/googletest
+
+LOCAL_SRC_FILES := $(GOOGLETEST_ROOT)/src/gtest_main.cc $(GOOGLETEST_ROOT)/src/gtest-all.cc
+##LOCAL_SRC_FILES := $(GTESTFILES:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES := $(GOOGLETEST_ROOT) $(GOOGLETEST_ROOT)/include
+
+LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_DISABLE_FATAL_LINKER_WARNINGS=true
+
+include $(BUILD_SHARED_LIBRARY)
+
+##############
+
+LOCAL_PATH := $(JNI_PATH)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PALISADEunit
+
+UNIT_FILE_LIST := jni/unittest/testmain2.cpp
 UNIT_FILE_LIST += $(shell find core/unittest -name '*.cpp')
 UNIT_FILE_LIST += $(shell find pke/unittest -name '*.cpp')
 LOCAL_SRC_FILES := $(UNIT_FILE_LIST:$(LOCAL_PATH)/%=%)
 
-LOCAL_C_INCLUDES := ../third-party/google-test/googletest ../third-party/google-test/googletest/include jni/gmp/$(APP_ABI) jni/ntl/prebuilt/$(APP_ABI) ../third-party/rapidjson/include core/lib pke/lib 
+GOOGLETEST_ROOT = $(ANDROID_NDK)/sources/third_party/googletest/googletest
+
+LOCAL_C_INCLUDES := $(GOOGLETEST_ROOT) $(GOOGLETEST_ROOT)/include jni/gmp/prebuilt/$(APP_ABI) jni/ntl/prebuilt/$(APP_ABI) ../third-party/rapidjson/include core/lib pke/lib 
+### LOCAL_C_INCLUDES := ../third-party/google-test/googletest ../third-party/google-test/googletest/include jni/gmp/prebuilt/$(APP_ABI) jni/ntl/prebuilt/$(APP_ABI) ../third-party/rapidjson/include core/lib pke/lib 
 
 LOCAL_CPP_FEATURES := rtti exceptions
 LOCAL_DISABLE_FATAL_LINKER_WARNINGS=true
 
 LOCAL_LDLIBS := -llog
+LOCAL_SHARED_LIBRARIES += gtest
 LOCAL_SHARED_LIBRARIES += ntl
 LOCAL_SHARED_LIBRARIES += gmp PALISADEcore PALISADEpke
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
 
