9dc50cf0a1d1c88e6e71afe27e12a1f20ed4db23
Antonis Papadimitriou <apapadimitriou@duality.cloud>
2019-03-04 11:47:50 -0500

    CPA-secure implementation of the BGV PRE scheme.

--- src/pke/lib/bgv-dcrtpoly-impl.cpp
+++ src/pke/lib/bgv-dcrtpoly-impl.cpp
@@ -31,4 +31,61 @@ namespace lbcrypto {
 template class LPCryptoParametersBGV<DCRTPoly>;
 template class LPPublicKeyEncryptionSchemeBGV<DCRTPoly>;
 template class LPAlgorithmBGV<DCRTPoly>;
+
+template <>
+LPEvalKey<DCRTPoly> LPAlgorithmPREBGV<DCRTPoly>::ReKeyGen(const LPPublicKey<DCRTPoly> newPK,
+	const LPPrivateKey<DCRTPoly> origPrivateKey) const
+{
+	// Get crypto context of new public key.
+	auto cc = newPK->GetCryptoContext();
+
+	// Create an evaluation key that will contain all the re-encryption key elements.
+	LPEvalKeyRelin<DCRTPoly> ek(new LPEvalKeyRelinImpl<DCRTPoly>(cc));
+
+	// Get crypto and elements parameters
+	const shared_ptr<LPCryptoParametersRLWE<DCRTPoly>> cryptoParamsLWE =
+		std::dynamic_pointer_cast<LPCryptoParametersRLWE<DCRTPoly>>(newPK->GetCryptoParameters());
+	const shared_ptr<typename DCRTPoly::Params> elementParams = cryptoParamsLWE->GetElementParams();
+
+	// Get parameters needed for PRE key gen
+	// r = relinWindow
+	usint relinWin = cryptoParamsLWE->GetRelinWindow();
+	// nBits = log2(q), where q: ciphertext modulus
+	usint nBits = elementParams->GetModulus().GetLengthForBase(2);
+
+	// K = log2(q)/r, i.e., number of digits in PRE decomposition
+	usint K = nBits / relinWin;
+	if (nBits % relinWin > 0)
+		K++;
+
+	// minus_skElem = -s(2^r)^i, s: secret key, r: relin window
+	DCRTPoly s = origPrivateKey->GetPrivateElement();
+	DCRTPoly minus_s = s.Negate();
+
+	std::vector<DCRTPoly> evalKeyElementsA(K);
+	std::vector<DCRTPoly> evalKeyElementsB(K);
+
+	// The re-encryption key is K ciphertexts, one for each -s(2^r)^i
+	for (usint i=0; i<K; i++) {
+		int numTowers = minus_s.GetAllElements().size();
+		BigInteger bb = BigInteger(1) << i*relinWin;
+		vector<NativeInteger> b(numTowers);
+
+		for (int j=0; j<numTowers; j++) {
+			auto mod = minus_s.ElementAtIndex(j).GetModulus();
+			auto bbmod = bb.Mod(mod);
+			b[j] = bbmod.ConvertToInt();
+		}
+
+		auto tmp = cc->GetEncryptionAlgorithm()->Encrypt(newPK, minus_s.Times(b));
+		evalKeyElementsA[i] = tmp->GetElements()[1];
+		evalKeyElementsB[i] = tmp->GetElements()[0];
+	}
+
+	ek->SetAVector(std::move(evalKeyElementsA));
+	ek->SetBVector(std::move(evalKeyElementsB));
+
+	return std::move(ek);
+}
+
 }
